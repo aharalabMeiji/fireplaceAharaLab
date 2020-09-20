74c1e8c733f00eb3651d48b06c5b1dbf70ef32a1
yamadamaya <8901eastward.shooting006@gmail.com>
2020-09-21 00:12:56 +0900

    シミュレーション中にゲームが終了するとExceptionでゲームが終わっちゃう問題があるのでそれは明日また頑張ろう

--- fireplaceAharaLab/agent_Maya.py
+++ fireplaceAharaLab/agent_Maya.py
@@ -24,17 +24,20 @@ def Maya_MCTS(game: ".game.Game"):
 		player=copyTree.current_player
 		print("--------------------simulate start!!----------------")
 		#探索編
-		candidates=getCandidates(copyTree)
+		candidates=getCandidates(copyTree,_getAllCandidates=True)
 		print("getCandidates")
-		if len(candidates)==0:
-			print("len(candidates)==0")
+		if len(candidates)==1:
+			print("len(candidates)==1")
 			return ExceptionPlay.VALID
 			pass
-		takingAction=try_montecarlo_tree_search(copyTree,candidates,_trialPerTree=1000);
+		takingAction=try_montecarlo_tree_search(copyTree,candidates,_trialPerTree=100);
 		print("--------------------simulate end!!------------------")
 		print(takingAction)
 		# iterate over our hand and play whatever is playable
 		#多分executeActionで大丈夫だろ
+		if takingAction.type is None:
+			return ExceptionPlay.VALID
+			pass
 		exc=executeAction(game, takingAction)
 		postAction(player)
 		if exc==ExceptionPlay.GAMEOVER:
@@ -77,20 +80,19 @@ def simulate_random_turn(game: ".game.Game"):
 	while True:
 		#getCandidate使った方が早くないか？
 		# iterate over our hand and play whatever is playable
-		simCandidates=getCandidates(game)
-		index=int(random.random()*(len(simCandidates)+1))
-		if index==len(simCandidates):
+		simCandidates=getCandidates(game,_getAllCandidates=True)
+		index=int(random.random()*len(simCandidates))
+		if simCandidates[index].type is None:
+			game.end_turn();
+			return ExceptionPlay.VALID
 			pass
+		exc=executeAction(game,simCandidates[index])
+		postAction(player)
+		if exc==ExceptionPlay.GAMEOVER:
+			return ExceptionPlay.GAMEOVER
 		else:
-			exc=executeAction(game,simCandidates[index])
-			postAction(player)
-			if exc==ExceptionPlay.GAMEOVER:
-				return ExceptionPlay.GAMEOVER
-			else:
-				continue
-				pass
-		game.end_turn();
-		return ExceptionPlay.VALID
+			continue
+			pass
 def simulate_random_game(game,trial=1)->"int":
 	retVal=0
 	for i in range(trial):
@@ -98,16 +100,8 @@ def simulate_random_game(game,trial=1)->"int":
 		winner=""
 		while True:
 			gameState=simulate_random_turn(simulating_game)
-			if simulating_game.state==State.COMPLETE:
-				if simulating_game.current_player.playstate == PlayState.WON:
-					winner=simulating_game.current_player.name
-					break;
-					pass
-				if simulating_game.current_player.playstate == PlayState.LOST:
-					winner=simulating_game.opponent.name
-					break;
-					pass
-				winner="DRAW"
+			if gameState==State.COMPLETE:
+				winner=judgeWinner(simulating_game)
 				break;
 		if winner=="Maya":
 			retVal+=1
@@ -143,43 +137,15 @@ def try_montecarlo_tree_search(_game,_candidates=[],_trialPerTree=10,_numOfTree=
 		root=Node(copyGame,None,None,_candidates)
 		print(_candidates)
 		for k in range(_trialPerTree):
-			currentNode=root
-			print("----------")
-			print("dig tree...")
-			while len(currentNode.untriedMoves)==0 and len(currentNode.childNodes)!=0:
-				currentNode=currentNode.selectChild()
-				print(currentNode.move)
-				print("->")
-				pass
-			print("digged!!")
-			print("expand child...")
-			if len(currentNode.untriedMoves)!=0:
-				#まだ探索してない枝がある
-				print("expanding...")
-				expandingAction=currentNode.choose_expanding_action()
-				try:
-					currentNode=currentNode.expandChild(expandingAction)
-				except GameOver:
-					#今gameoverに勝者の情報は入っていないので注意して
-					newChild=Node(None,expandingAction,currentNode,[])
-					if winner=="Maya":
-						newChild.setScore(1)
-						pass
-					else:
-						newChild.setScore(0)
-					currentNode.childNodes.append(newChild)
-					newChild.backPropagate()
-					continue
-				print("done")
-				pass
-			else:
-				currentNode.backPropagate()
-				continue
-				pass
-			print("is it ended?")
-			result=simulate_random_game(currentNode.gameTree)
-			currentNode.backPropagate(result)
-			pass
+			current_node = root;
+			while len(current_node.untriedMoves) == 0 and len(current_node.childNodes) != 0:
+				current_node = current_node.selectChild();
+			if len(current_node.untriedMoves) != 0:
+				expanding_action=current_node.choose_expanding_action()
+				current_node = current_node.expandChild(expanding_action);
+			result = current_node.simulate();
+			current_node.backPropagate(result);
+
 		visitScores=list(map(lambda node:myActionValue(node.move,node.visits),root.childNodes))
 		totalScores=addActionValues(totalScores,visitScores)
 		print("totalScores")
@@ -220,6 +186,13 @@ def get_cardList(card_class:CardClass,exclude=[]):
 		collection.append(cls)
 	pass
 	return collection
+def judgeWinner(game):
+	if game.current_player.playstate == PlayState.WON:
+		return game.current_player.name
+	if game.current_player.playstate == PlayState.LOST:
+		return game.current_player.opponent.name
+	return 'DRAW'
+	pass
 class Node(object):
 	"""docstring for Node"""
 	def __init__(self, gameTree,move,parent,_candidates):
@@ -243,6 +216,13 @@ class Node(object):
 		self.originalGame=copy.deepcopy(self.gameTree)
 		executeAction(self.gameTree,action)
 		postAction(self.gameTree.current_player)
+		if self.gameTree.state==State.COMPLETE:
+			child=Node(self.gameTree,action,self,[])
+			return child
+			pass
+		elif action.type is None:
+			self.gameTree.end_turn()
+			pass
 		child=Node(self.gameTree,action,self,getCandidates(self.gameTree))
 		self.childNodes.append(child)
 		self.gameTree=self.originalGame
@@ -252,7 +232,12 @@ class Node(object):
 		return self.untriedMoves.pop(index)		
 		pass
 	def simulate(self):
-		return simulate_random_game(self.gameTree)
+		if self.gameTree.state==State.COMPLETE:
+			self.score=judgeWinner(self.gameTree)
+			pass
+		else:
+			self.score=simulate_random_game(self.gameTree)
+		return self.score
 		pass
 	def backPropagate(self,result=None):
 		self.addVal=self.score
@@ -265,7 +250,4 @@ class Node(object):
 			pass
 		else:
 			self.parent.backPropagate(self.addVal)
-		pass
-	def setScore(self,_score):
-		self.score=_score
 		pass
