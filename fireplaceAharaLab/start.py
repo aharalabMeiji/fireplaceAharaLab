d219fbc232eb054d1654ecd7b82246982562a5e6
DESKTOP-G27VTKQ\ahara2017 <ahara@meiji.ac.jp>
2020-10-25 08:50:15 +0900

    Update start.py

--- fireplaceAharaLab/start.py
+++ fireplaceAharaLab/start.py
@@ -20,9 +20,9 @@ def main():
 	#ランダムプレーヤー
 	Random=StandardAgent("Standard",StandardAgent.StandardRandom, myClass=CardClass.HUNTER) 
 	#ベクトルプレーヤー。意外と強い。この人とサシで勝負して勝てるくらいが一応の目安。
-	Vector=StandardVectorAgent("Vector",StandardVectorAgent.StandardStep1\
-		,myOption=[3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8]\
-		,myClass=CardClass.HUNTER) 		
+	#Vector=StandardVectorAgent("Vector",StandardVectorAgent.StandardStep1\
+	#	,myOption=[3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8]\
+	#	,myClass=CardClass.HUNTER) 		
 
 	# Maya : モンテカルロによる読み切り
 	#Maya=Agent("Maya",Maya_MCTS)
@@ -36,14 +36,14 @@ def main():
 	#takasho001=takasho001Agent("Takasho",takasho001Agent.takashoAI)
 
 	# 言葉で戦略を組み立てるエージェント by Ahara
-	from agent_word_strategy import WordStrategyAgent
-	WordStrategy = WordStrategyAgent("WS", WordStrategyAgent.agent_word_strategy\
-		,myOption=[WS.ミニョンで敵ヒーローの体力を削る, WS.呪文を使えるなら呪文, WS.ランダムにプレー]\
-		,myClass=CardClass.PRIEST)
+	#from agent_word_strategy import WordStrategyAgent
+	#WordStrategy = WordStrategyAgent("WS", WordStrategyAgent.agent_word_strategy\
+	#	,myOption=[WS.ミニョンで敵ヒーローの体力を削る, WS.呪文を使えるなら呪文, WS.ランダムにプレー]\
+	#	,myClass=CardClass.PRIEST)
 
 	#AngryCat ： シンプルに選択するアルゴリズム
-	from agent_AngryCat import AngryCatAgent
-	AngryCat = AngryCatAgent("AngryCat", AngryCatAgent.AngryCatAI)
+	#from agent_AngryCat import AngryCatAgent
+	#AngryCat = AngryCatAgent("AngryCat", AngryCatAgent.AngryCatAI)
 
 	#HunterCat : faceHunter専用のエージェント
 	from agent_HunterCat import HunterCatAgent
@@ -52,13 +52,13 @@ def main():
 	####################################################################
 
 	#ゲームプレイ(きまったゲーム数を対戦し、勝ち数を数える)
-	#play_set_of_games(Human, Random, gameNumber=1, debugLog=True)
+	play_set_of_games(Human, HunterCat, gameNumber=1, debugLog=True)
 	#デッキを固定しての対戦
 	#play_set_of_games(Human, Random, BigDeck.faceHunter, BigDeck.faceHunter, gameNumber=10, debugLog=True)
 
 	#総当たり戦
-	from competition import play_round_robin_competition
-	play_round_robin_competition([Random,Vector,AngryCat,HunterCat],matchNumber=1)
+	#from competition import play_round_robin_competition
+	#play_round_robin_competition([Random,Vector,AngryCat,HunterCat],matchNumber=1)
 
 	#特定の2枚のカードのシナジーを調べる(idea by Maya)
 	#from card_pair import investigate_card_pair, find_card_pair
