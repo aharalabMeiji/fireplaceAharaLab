19206308717144582c2377d94e8c08df2ab72edd
takasho002 <rezerozero2@gmail.com>
2020-10-25 15:43:40 +0900

    a

--- agent_takasho001.py
+++ agent_takasho001.py
@@ -1,5 +1,6 @@
 #### takashoのAI #####
 import random
+from utils import *
 import numpy as np
 import copy
 from fireplace.exceptions import GameOver
@@ -10,5 +11,20 @@ from enum import IntEnum
 
 from agent_Standard import postAction, StandardRandom
 
-def takasho001AI(thisGame: Game,debugLog=True):
-	# とりあえず
+
+
+
+
+class agent_takasho(Agent):    
+     def __init__(self, myName: str, myFunction, myOption = [], myClass: CardClass = CardClass.HUNTER, rating =1000 ):
+          super().__init__(myName, myFunction, myOption, myClass, rating )
+     def agent_takasho001(game: Game, option=[], gameLog=[], debugLog=False):
+          while True:
+			  myCandidates = getCandidates(Game)
+               for choice in myCandidates:
+				   if choice.type==BlockType.ATTACK and choice.target==player.opponent.hero:
+					   executeAction(choice)
+					   postAction(player)
+					   break
+					else:
+						return
