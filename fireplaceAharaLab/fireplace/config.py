13481ce413519ae4db99f0ae2d35abc787746501
Kazushi Ahara <ahara@meiji.ac.jp>
2021-07-10 08:16:26 +0900

    Update config.py

--- fireplaceAharaLab/fireplace/config.py
+++ fireplaceAharaLab/fireplace/config.py
@@ -6,6 +6,7 @@ class Config(IntEnum):# ()内はデフォルト値
     FSFIXED=1 # 先攻と後攻を固定(0) YES:>0 NO:0
     COIN=1 # 後攻にコインを与えるか(1) YES:>0 NO:0
     EX_CARD=1 # 後攻にカードを与えるか(1) YES:>0 NO:0
+    # 以下はplay_set_of_games()のオプションへ移動
     #HEROHPOPTION=30 #ヒーロー体力(30)
     #P1MAXMANA=1 # 先攻マナ(1) 1~10
     #P2MAXMANA=1 # 後攻マナ(1) 1~10
