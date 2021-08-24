edfd1a3477d5aa12f929be5344bd7d1a1852c135
Kazushi Ahara <ahara@meiji.ac.jp>
2021-08-24 23:34:13 +0900

    darkmoon-hunterを埋めた。検証はまだ

--- fireplaceAharaLab/fireplace/cards/darkmoon/hunter.py
+++ fireplaceAharaLab/fireplace/cards/darkmoon/hunter.py
@@ -1,9 +1,19 @@
 from ..utils import *
 
+class DMF_083_Corrupt(TargetedAction):
+    TARGET = ActionArg()
+    CORRUPTED = CardArg()
+    def do(self, source, target, corrupted):
+        if target.cost > source.cost:
+            for card in corrupted:
+                Summon(source.controller, card).trigger(source.controller)
+            Destroy(source).trigger(source.controller)
+        pass
+
 class DMF_083:
     """ Dancing Cobra
     [Corrupt:] Gain [Poisonous]. """
-    #
+    events = Play(CONTROLLER).on(DMF_083_Corrupt(Play.CARD,'DMF_083t'))
     pass
 
 class DMF_083t:
@@ -12,22 +22,33 @@ class DMF_083t:
     #
     pass
 
+class DMF_084_Action(TargetedAction):
+    TARGET=ActionArg()
+    def do(self, source, target):
+        _player = target
+        _minionList=_player.__myDeathLog__
+        _count =0
+        for _card in _minionList:
+            if _card.deathrattle and _count<3:
+                Summon(_player, Copy(ID(_card))).trigger(_player)
+                _count += 1
+
 class DMF_084:
     """ Jewel of N'Zoth
     Summon three friendly [Deathrattle] minions that died this game. """
-    #
+    play = DMF_084_Action(CONTROLLER)
     pass
 
 class DMF_085:
     """ Darkmoon Tonk
-    [Deathrattle:] Fire four  missiles at random enemies that deal 2 damage each. """
-    #
+    [Deathrattle:] Fire four missiles at random enemies that deal 2 damage each. """
+    deathrattle = Hit(RANDOM(ENEMY_CHARACTERS),2) * 4
     pass
 
 class DMF_086:
     """ Petting Zoo
     Summon a 3/3 Strider. Repeat for each [Secret] you control. """
-    #
+    play = Summon(CONTROLLER,'DMF_086e'), Summon(CONTROLLER,'DMF_086e') * Count(FRIENDLY_SECRETS)
     pass
 
 class DMF_086e:
@@ -36,28 +57,37 @@ class DMF_086e:
     #
     pass
 
+class DMF_087_Action(TargetedAction):
+    CONTROLLER=ActionArg()
+    DEFENDER=ActionArg()
+    def do(self,source,controller,defender):
+        excess = source.atk - defender.health
+        if excess > 0:
+            Hit(controller.opponent.hero, excess).trigger(controller)
+
 class DMF_087:
     """ Trampling Rhino
-    [Rush]. After this attacksand kills a minion, excess damage hits the enemy hero. """
-    #
+    [Rush]. After this attacks and kills a minion, excess damage hits the enemy hero. """
+    events = Attack(SELF,ENEMY_MINIONS).on(DMF_087_Action(CONTROLLER,Attack.DEFENDER))
     pass
 
 class DMF_088:
     """ Rinling's Rifle
     After your hero attacks, [Discover] a [Secret] and cast it. """
-    #
+    events = Attack(FRIENDLY_HERO).on(GenericChoicePlay(CONTROLLER, RANDOM(SECRET)*3))
     pass
 
 class DMF_089:
     """ Maxima Blastenheimer
-    [Battlecry:] Summon a minionfrom your deck. It attacks theenemy hero, then dies. """
-    #
+    [Battlecry:] Summon a minion from your deck. It attacks the enemy hero, then dies. """
+    play = Summon(CONTROLLER,RANDOM(FRIENDLY_DECK + MINION)).then(Attack(Summon.CARD,ENEMY_HERO), Destroy(Summon.CARD))
     pass
 
 class DMF_090:
     """ Don't Feed the Animals
     Give all Beasts in your hand +1/+1.[Corrupt:] Give them +2/+2 instead. """
-    #
+    play = Buff(FRIENDLY_HAND + BEAST,'DMF_090e2')
+    events = Play(CONTROLLER).on(DMF_083_Corrupt(Play.CARD,'DMF_090t'))
     pass
 
 class DMF_090e:
@@ -75,35 +105,36 @@ class DMF_090e2:
 class DMF_090t:
     """ Don't Feed the Animals
     [Corrupted]Give all Beasts in your hand +2/+2. """
-    #
+    play = Buff(FRIENDLY_HAND + BEAST,'DMF_090e')
     pass
 
 class DMF_122:
     """ Mystery Winner
     [Battlecry:] [Discover] a [Secret.] """
-    #
+    play = GenericChoice(CONTROLLER, RANDOM(SECRET)*3)
     pass
 
 class DMF_123:
     """ Open the Cages
-    [Secret:] When yourturn starts, if you control two minions, summon anAnimal Companion. """
-    #
+    [Secret:] When your turn starts, if you control two minions, summon an Animal Companion. """
+    secret = OWN_TURN_BEGIN.on(Count(FRIENDLY_MINIONS)>=2 & (Reveal(SELF), Summon(CONTROLLER,'NEW1_031')))
     pass
 
 class DMF_734e:
     """ Greybud
     [Deathrattle:] Summon Greybough. """
-    #
+    deathrattle = Summon(CONTROLLER,'DMF_734')
     pass
 
 class YOP_027:
     """ Bola Shot
     Deal $1 damage to a minion and $2 damage to its neighbors. """
-    #
+    requirements = { PlayReq.REQ_TARGET_TO_PLAY:0, PlayReq.REQ_MINION_TARGET:0, PlayReq.REQ_ENEMY_TARGET:0}
+    play = Hit(TARGET, 1), Hit(TARGET_ADJACENT,2)
     pass
 
 class YOP_028:
     """ Saddlemaster
     After you play a Beast, add_a random Beast to_your hand. """
-    #
+    events = Play(CONTROLLER, BEAST).on(Give(CONTROLLER,RANDOM(BEAST)))
     pass
