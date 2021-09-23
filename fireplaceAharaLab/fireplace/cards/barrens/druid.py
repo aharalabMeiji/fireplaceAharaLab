e37b696dffd9da00d7bdb20e40e6947e5d18f031
Kazushi Ahara <ahara@meiji.ac.jp>
2021-09-24 07:01:59 +0900

    WC_004:#OK

--- fireplaceAharaLab/fireplace/cards/barrens/druid.py
+++ fireplaceAharaLab/fireplace/cards/barrens/druid.py
@@ -131,7 +131,7 @@ BAR_549e=buff(2,2)# <2>[1525]
 """ Everbark
 +2/+2. """
 
-class BAR_720:# <2>[1525]
+class BAR_720:#OK <2>[1525]
 	""" Guff Runetotem
 	After you cast a Nature spell, give another friendly minion +2/+2. """
 	events = Play(CONTROLLER, SPELL + NATURE).on(Buff(RANDOM(FRIENDLY_MINIONS - SELF),'BAR_720e'))
@@ -140,12 +140,11 @@ BAR_720e=buff(2,2)# <2>[1525]
 """ Guff's Buff
 +2/+2. """
 
-class WC_004:# <2>[1525]
+class WC_004:#OK <2>[1525]
 	""" Fangbound Druid
 	[Taunt][Deathrattle:] Reduce the Cost of a Beast in your hand by (2). """
-	deathrattle = Buff(FRIENDLY_HAND + BEAST,'WC_004t')
+	deathrattle = Buff(RANDOM(FRIENDLY_HAND + BEAST),'WC_004t')
 	pass
-
 WC_004t=buff(cost=-2)# <2>[1525]
 """ Nightmare Trapped
 Costs (2) less. """
