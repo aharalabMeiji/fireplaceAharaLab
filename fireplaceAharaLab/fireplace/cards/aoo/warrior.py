4a575ca0317f183f8d9fe0632316b8382709da6b
Kazushi Ahara <ahara@meiji.ac.jp>
2021-09-29 23:45:26 +0900

    aoo_warrior始めた

--- fireplaceAharaLab/fireplace/cards/aoo/warrior.py
+++ fireplaceAharaLab/fireplace/cards/aoo/warrior.py
@@ -1,31 +1,46 @@
 from ..utils import *
 
-AOO_Warrior=['BT_117','BT_120','BT_121','BT_123','BT_123t',
-'BT_124','BT_124e','BT_138','BT_140','BT_233','BT_249','BT_781',]
+AOO_Warrior=['BT_120','BT_121','BT_123','BT_123t',
+'BT_138','BT_140','BT_233','BT_249',]
+#bigWarrior:'BT_117','BT_124','BT_124e','BT_781',
+
 #'BTA_BOSS_04h','BTA_BOSS_04s','BTA_BOSS_06h',
 #'BTA_BOSS_08h','BTA_BOSS_08p','BTA_BOSS_11p','BTA_BOSS_12t',
 #'BTA_BOSS_14h2','BTA_BOSS_14p2','BTA_BOSS_23p',
 #'Prologue_Azzinoth','Story_09_BonechewerRaider',]
 
-class BT_117:# <10>[1414]
-	""" Bladestorm
-	Deal $1 damage to all minions. Repeat until one dies. """
-	#
-	pass
+#class BT_117:# <10>[1414]
+#	""" Bladestorm
+#	Deal $1 damage to all minions. Repeat until one dies. """
+#    def play(self):
+#        controller = self.controller
+#        game = controller.game
+#        before = game.board
+#        for i in range(100):# 1000? lol
+#            if len(game.board)>0:
+#                for card in game.board:
+#                    Hit(card, 1).trigger(controller)
+#                for card in game.board:
+#                    if card.health == 0:
+#                        return
+#            else:
+#                return
+#        pass
+#    pass
 
 class BT_120:# <10>[1414]
 	""" Warmaul Challenger
-	[Battlecry:] Choosean enemy minion.Battle it to the death! """
-	#
+	[Battlecry:] Choose an enemy minion.Battle it to the death! """
+	requirements = { PlayReq.REQ_TARGET_TO_PLAY:0, PlayReq.REQ_MINION_TARGET:0 }#
 	pass
 
-class BT_121:# <10>[1414]
+class BT_121:# <10>[1414] #### Find a 3/2 Axe
 	""" Imprisoned Gan'arg
 	[Dormant] for 2 turns.When this awakens,equip a 3/2 Axe. """
 	#
 	pass
 
-class BT_123:# <10>[1414]
+class BT_123:# <10>[1414] ## BT_123t
 	""" Kargath Bladefist
 	[Rush][Deathrattle:] Shuffle'Kargath Prime'into your deck. """
 	#
@@ -37,19 +52,16 @@ class BT_123t:# <10>[1414]
 	#
 	pass
 
-class BT_124:# <10>[1414]
-	""" Corsair Cache
-	Draw a weapon.Give it +1 Durability. """
-	#
-	pass
+#class BT_124:###OK
+#	"""Corsair Cache
+#	Draw a weapon. Give it +1 Durability."""
+#	play = ForceDraw(RANDOM(FRIENDLY_DECK + WEAPON)).then(
+#		Buff(ForceDraw.TARGET, "BT_124e"))
+#	pass
+#BT_124e = buff(health=1)
 
-class BT_124e:# <10>[1414]
-	""" Void Sharpened
-	+1 Durability. """
-	#
-	pass
 
-class BT_138:# <10>[1414]
+class BT_138:# <10>[1414] # no enchantment
 	""" Bloodboil Brute
 	[Rush]Costs (1) less for each damaged minion. """
 	#
@@ -64,6 +76,7 @@ class BT_140:# <10>[1414]
 class BT_233:# <10>[1414]
 	""" Sword and Board
 	Deal $2 damage to a minion. Gain 2 Armor. """
+	requirements = { PlayReq.REQ_TARGET_TO_PLAY:0, PlayReq.REQ_MINION_TARGET:0 }#
 	#
 	pass
 
@@ -73,81 +86,13 @@ class BT_249:# <10>[1414]
 	#
 	pass
 
-class BT_781:# <10>[1414]
-	""" Bulwark of Azzinoth
-	Whenever your hero wouldtake damage, this loses_1 Durability instead. """
-	#
-	pass
-
-class BTA_BOSS_04h:# <10>[1414]
-	""" Baltharak
-	 """
-	#
-	pass
-
-class BTA_BOSS_04s:# <10>[1414]
-	""" Entombed in Rust
-	Baltharak is corruptingan Outcast to join theRusted Legion. Free Karnukand he will join you! """
-	#
-	pass
-
-class BTA_BOSS_06h:# <10>[1414]
-	""" Burgrak Cruelchain
-	 """
-	#
-	pass
-
-class BTA_BOSS_08h:# <10>[1414]
-	""" Mother Shahraz
-	 """
-	#
-	pass
-
-class BTA_BOSS_08p:# <10>[1414]
-	""" Parry and Riposte
-	[Passive Hero Power]The first time you are damaged each turn, prevent it and counterattack! """
-	#
-	pass
-
-class BTA_BOSS_11p:# <10>[1414]
-	""" Rocket Wings
-	[Passive Hero Power]Your minions have [Rush] and [Windfury]. """
-	#
-	pass
-
-class BTA_BOSS_12t:# <10>[1414]
-	""" Rusted Fel Orc
-	After this minion attacks, give all other Rusted Fel Orcs +1 Attack. """
-	#
-	pass
-
-class BTA_BOSS_14h2:# <10>[1414]
-	""" Rusted Fel Reaver
-	 """
-	#
-	pass
-
-class BTA_BOSS_14p2:# <10>[1414]
-	""" Hulking Monstrosity
-	[Passive Hero Power]Your minions have +2 Attack. Fel Reavers have +10 Attack instead. """
-	#
-	pass
-
-class BTA_BOSS_23p:# <10>[1414]
-	""" Parry and Riposte
-	[Passive Hero Power]The first time you are damaged each turn, prevent it and counterattack! """
-	#
-	pass
-
-class Prologue_Azzinoth:# <10>[1414]
-	""" Azzinoth
-	 """
-	#
-	pass
-
-class Story_09_BonechewerRaider:# <10>[1414]
-	""" Bonechewer Raider
-	[Battlecry:] If there is a damaged minion, gain +1/+1 and [Rush]. """
-	#
-	pass
-
+#class BT_781:###OK
+#	"""Bulwark of Azzinoth
+#	Whenever your hero would take damage, this loses 1 Durability instead.
+#	"""
+#	# see AT_124
+#	#update = Refresh(FRIENDLY_HERO, {GameTag.HEAVILY_ARMORED: True})
+#	events = Predamage(FRIENDLY_HERO).on(
+#		 Predamage(FRIENDLY_HERO, 0), Hit(SELF, 1))
+#	# BuffじゃなくてHit??
+#	pass
--- fireplaceAharaLab/fireplace/cards/core/warrior.py
+++ fireplaceAharaLab/fireplace/cards/core/warrior.py
@@ -1,5 +1,9 @@
 from ..utils import *
 
+CORE_WARRIOR=['CORE_CS2_106','CORE_CS2_108','CORE_EX1_084','CORE_EX1_391','CORE_EX1_400','CORE_EX1_402','CORE_EX1_411','CORE_EX1_414','CORE_EX1_603','CORE_EX1_604','CORE_GVG_053','CS3_008','CS3_008e','CS3_009','CS3_030',]
+#['CORE_EX1_407','CORE_EX1_410',]#bigWarrior
+
+
 class CORE_CS2_106:# <10>[1637]
 	""" Fiery War Axe
 	 """
@@ -9,37 +13,49 @@ class CORE_CS2_106:# <10>[1637]
 class CORE_CS2_108:# <10>[1637]
 	""" Execute
 	Destroy a damaged enemy minion. """
-	#
+	requirements = {
+		PlayReq.REQ_DAMAGED_TARGET: 0,
+		PlayReq.REQ_ENEMY_TARGET: 0,
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Destroy(TARGET)
 	pass
 
 class CORE_EX1_084:# <10>[1637]
 	""" Warsong Commander
 	After you summon another minion, give it [Rush]. """
-	#
+	events = Summon(FRIENDLY - SELF).on(Buff(Summon.CARD,"EX1_084e"))
 	pass
+EX1_084e = buff(rush=True)
 
 class CORE_EX1_391:# <10>[1637]
 	""" Slam
 	Deal $2 damage to a minion. If it survives, draw a card. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(TARGET, 2), Dead(TARGET) | Draw(CONTROLLER)
 	pass
 
 class CORE_EX1_400:# <10>[1637]
 	""" Whirlwind
 	Deal $1 damage to ALL_minions. """
-	#
+	play = Hit(ALL_MINIONS, 1)
 	pass
 
 class CORE_EX1_402:# <10>[1637]
 	""" Armorsmith
 	Whenever a friendly minion_takes damage, gain 1 Armor. """
-	#
+	events = Damage(FRIENDLY_MINIONS).on(GainArmor(FRIENDLY_HERO, 1))
 	pass
 
 #class CORE_EX1_407:# <10>[1637]# -> cards.bigWarrior.bigWarrior
 #	""" Brawl
 #	Destroy all minions except one. <i>(chosen randomly)</i> """
-#	#
+#    requirements = {PlayReq.REQ_MINIMUM_TOTAL_MINIONS: 2}
+#    play = (
+#        Find(ALL_MINIONS + ALWAYS_WINS_BRAWLS) &
+#        Destroy(ALL_MINIONS - RANDOM(ALL_MINIONS + ALWAYS_WINS_BRAWLS)) |
+#        Destroy(ALL_MINIONS - RANDOM_MINION)#たぶんこれだけでよい、と思う。
+#    )
 #	pass
 
 #class CORE_EX1_410:# <10>[1637] -> cards.bigWarrior.bigWarrior
@@ -52,49 +68,64 @@ class CORE_EX1_402:# <10>[1637]
 class CORE_EX1_411:# <10>[1637]
 	""" Gorehowl
 	Attacking a minion costs 1 Attack instead of 1 Durability. """
-	#
+	update = Attacking(FRIENDLY_HERO, MINION) & Refresh(SELF, buff="EX1_411e")
+	events = Attack(FRIENDLY_HERO, MINION).after(Buff(SELF, "EX1_411e2"))
 	pass
+EX1_411e = buff(immune=True)
+EX1_411e2 = buff(atk=-1)
 
 class CORE_EX1_414:# <10>[1637]
 	""" Grommash Hellscream
 	[Charge]Has +6 Attack while damaged. """
-	#
+	enrage = Refresh(SELF, buff="EX1_414e")
 	pass
+EX1_414e = buff(atk=6)
 
 class CORE_EX1_603:# <10>[1637]
 	""" Cruel Taskmaster
 	[Battlecry:] Deal 1 damage to a minion and give it +2_Attack. """
-	#
+	requirements = {
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_NONSELF_TARGET: 0,
+		PlayReq.REQ_TARGET_IF_AVAILABLE: 0}
+	play = Buff(TARGET, "EX1_603e"), Hit(TARGET, 1)
 	pass
+EX1_603e = buff(atk=2)
 
 class CORE_EX1_604:# <10>[1637]
 	""" Frothing Berserker
 	Whenever a minion takes damage, gain +1 Attack. """
-	#
+	events = Damage(ALL_MINIONS).on(Buff(SELF, "EX1_604o"))
 	pass
+EX1_604o = buff(atk=1)
 
 class CORE_GVG_053:# <10>[1637]
 	""" Shieldmaiden
 	[Battlecry:] Gain 5 Armor. """
-	#
+	play = GainArmor(FRIENDLY_HERO, 5)
 	pass
 
 class CS3_008:# <10>[1637]
 	""" Bloodsail Deckhand
-	[Battlecry:] The nextweapon you play costs(1) less. """
-	#
+	[Battlecry:] The next weapon you play costs(1) less. """
+	update = BuffOnce(FRIENDLY_WEAPON,'CS3_008e')
 	pass
 
-class CS3_008e:# <10>[1637]
+class CS3_008e:# <10>[1637]## updateが弱いかも
 	""" To Arrrms!
 	Your next weapon costs (1) less. """
-	#
+	cost = lambda self, i: max(i-1,0)
+	events = Play(CONTROLLER, WEAPON).on(Destroy(SELF))
 	pass
 
 class CS3_009:# <10>[1637]
 	""" War Cache
 	Add a random Warrior minion, spell, and weapon to your hand. """
-	#
+	play = (
+		Give(CONTROLLER, RANDOM(MINION + WARRIOR)),
+		Give(CONTROLLER, RANDOM(SPELL + WARRIOR)),
+		Give(CONTROLLER, RANDOM(WEAPON + WARRIOR)),
+		)
 	pass
 
 class CS3_030:# <10>[1637]
@@ -102,4 +133,3 @@ class CS3_030:# <10>[1637]
 	[Rush] """
 	#
 	pass
-
