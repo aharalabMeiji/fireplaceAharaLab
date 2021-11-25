85ed9acb7ed34e3dd3b8fc28efc227e269f1d4c5
Kazushi Ahara <ahara@meiji.ac.jp>
2021-11-26 07:42:12 +0900

    neutralファイルを作ってみた。

---
+++ fireplaceAharaLab/fireplace/cards/alterac/neutral.py
@@ -0,0 +1,226 @@
+from ..utils import *
+
+class ALT_XX1:
+	""" Drek'Thar (4/4/4)
+	[Battlecry]: Id this costs more than every minion in your deck, summon 2 of them"""
+	pass
+
+class ALT_XX2:
+	""" Vanndar Storm (4/4/4)
+	[Battlecry]: If this costs less than every minions in your deck, reduce their cost by (3)."""
+	pass
+
+class ALT_XX3:
+	""" Wildheart Guff(5/*/5)
+	[Battlecry]: Set your maximum Mana to 20.  Gain a Mana Crystal Draw a card."""
+	pass
+
+class ALT_XX4:
+	""" Siphon Mana(2)
+	Deal 2 damage.  [Honorable kill]: Reduce the Cost of spells in your hand by (1)"""
+	pass
+
+class ALT_XX5:
+	""" Snowfall Graceyard(3)
+	Your Deathrattles trigger twice.  Lasts 3 turns """
+	pass
+
+class ALT_NEU1:
+	""" Blood Guard (5/4/7)
+	Whenever this minion takes damage, give your minions +1 Attack. """
+	pass
+
+class ALT_NEU2:
+	""" Bunker Sergeant (3/2/4)
+	[Battlecry]: If your opponent has 2 or more minions, deal 1 damage to all enemy minions."""
+	pass
+
+class ALT_NEU3:
+	""" Direwolf Commander (3/2/5)
+	[Honorable Kill]: Summon a 2/2 Wolf with Stealth """
+	pass
+
+class ALT_NEU4:
+	""" Frantic Hippogryph (5/3/7)
+	[Rush]. [Honorable Kill]: Gain [Windfury]. """
+	pass
+
+class ALT_NEU5:
+	""" Frostwolf Warmaster (4/3/5)
+	Costs (1) less for each card you've played this turn."""
+	pass
+
+class ALT_NEU6:
+	""" Gankster (2/4/2)
+	[Stealth] After your opponents plays a minion, attack it"""
+	pass
+
+class ALT_NEU_7:
+	""" Herald of Lokholar (4/3/5)
+	[Battlecry]: Draw a Frost spell."""
+	pass
+
+class ALT_NEU_8:
+	"""Humongous Owl(7/8/4)
+	Deathrattle: Deal 8 damage to a random enemy."""
+	pass
+
+class ALT_NEU_9:
+	""" Ice Revenant (4/4/5)
+	Whenever you cast a Frost spell, gain +2/+2. """
+	pass
+
+class ALT_NEU_10:
+	""" Icehoof Protector (6/2/10)
+	[Taunt] [Freeze] any character damaged by this minion."""
+	pass
+
+class ALT_NEU_11:
+	""" Irondeep Trogg (1/1/2)
+	After your opponent casts a spell, summon a copy of this."""
+	pass
+
+class ALT_NEU_12:
+	""" Kobold Taskmaster (3/2/4)
+	[Battlecry]: Add 2 Armor Scraps to your hand that give +2 Health to a minion."""
+	pass
+
+class ALT_NEU_13:
+	""" Korrak the Bloodrager (4/3/5)
+	Deathrattle: If this wasn't Honorably Killed, resummon Korrak."""
+	pass
+
+class ALT_NEU_14:
+	""" Legionnaire (6/9/3)
+	Deathrattle: Give all minions in your hand +2/+2. """
+	pass
+
+class ALT_NEU_15:
+	""" Piggyback Imp (3/1/1) deamon
+	Deathrattle: Summon a 4/1 Imp. """
+	pass
+
+class ALT_NEU_16:
+	""" Reflecto Engineer (3/2/4)
+	[Battlecry]: Swap the Attack and Health of all minions in both players' hands."""
+	pass
+
+class ALT_NEU_17:
+	""" Ram Commander (2/2/2)
+	[Battlecry]: Add two 1/1 Rams with Rush to your hand."""
+	pass
+
+class ALT_NEU_18:
+	""" Snowblind Harpy (3/3/4)
+	[Battlecry]: If you're holding a Frost spell, gain 5 Armor."""
+	pass
+
+class ALT_NEU_19:
+	""" Stormpike Marshal (4/2/6)
+	[Taunt] If you took 5 or more damage on your opponent's turn, this costs (1)."""
+	pass
+
+class ALT_NEU_:
+	""" Stormpike Quartermaster (2/2/2)
+	After you cast a spell, give a random minion in your hand +1/+1."""
+	pass
+
+class ALT_NEU_:
+	""" Tower Sergeant (4/4/4)
+	[Battlecry]: If you control at least 2 other minions, gain +2/+2."""
+	pass
+
+class ALT_NEU_:
+	""" Troll Centurion (8/8/8)
+	[Rush]. [Honorable Kill]: Deal 8 damage to the enemy hero."""
+	pass
+
+class ALT_NEU_:
+	""" Sneaky Scout (2/3/2)
+	[Stealth] [Honorable Kill]: Your next Hero Power costs (0). """
+	pass
+
+class ALT_NEU_:
+	""" Corporal ( 2/2/3)
+	Honorable Kill: Give your other minions Divine Shield."""
+	pass
+
+class ALT_NEU_:
+	""" Frozen Mammoth (4/6/7)
+	This is [Frozen] until you cast a Fire spell."""
+	pass
+
+class ALT_NEU_:
+	""" Popsicooler (3/3/3) Mech
+	[Deathrattle]: [Freeze] two random enemy minions."""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+class ALT_NEU_:
+	"""
+	"""
+	pass
+
+
+
+
+
