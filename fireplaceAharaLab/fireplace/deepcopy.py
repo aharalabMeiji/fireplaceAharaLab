f72c7cf4aa52d2b74cd9ff770929bbeece271766
Kazushi Ahara <ahara@meiji.ac.jp>
2021-12-28 09:01:45 +0900

    update 20211228

--- fireplaceAharaLab/fireplace/deepcopy.py
+++ fireplaceAharaLab/fireplace/deepcopy.py
@@ -233,14 +233,14 @@ def copy_playerattr(oldPlayer, newPlayer):
 	#play_targets
 	#targets
 	# player's attr 
-	playerAttrs = ['cards_drawn_this_turn','_max_mana','playstate','zone',
+	playerAttrs = ['cards_drawn_this_turn','_max_mana','max_mana','playstate','zone',
 				'first_player','mulligan_state','turn_start',
 				'minions_played_this_turn','combo','cards_played_this_turn',
 				'spell_and_damage','guardians_legacy','spellpower_option',
 				'choiceStrategy','lost_in_the_park','zone',
 				'piece_of_cthun','_death_log','_play_log','_damage_log',
 				'_activate_log','_summon_log','_reveal_log','carry_cards',
-				'last_card_played','used_mana','times_spell_played_this_game',
+				'last_card_played','used_mana','overload_locked','temp_mana','times_spell_played_this_game',
 				'times_spells_played_this_turn','spells_played_this_turn',
 				'times_hero_power_used_this_game','times_card_to_play_out_of_deck',]
 	for attr in playerAttrs:
