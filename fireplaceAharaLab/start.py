5a13cc5eacb577815be3ede5aa7eeb51c5a613f5
Kazushi Ahara <ahara@meiji.ac.jp>
2021-12-29 16:36:13 +0900

    ほんちゃん？

--- fireplaceAharaLab/start.py
+++ fireplaceAharaLab/start.py
@@ -59,7 +59,7 @@ def main():
 	#ゲームプレイ(きまったゲーム数を対戦し、勝ち数を数える)
 	#from utils import BigDeck
 	##BigDeck.faceHunter, BigDeck.clownDruid, BigDeck.bigWarrior
-	a,b,c = play_set_of_games(Vector1, Vector2, deck1=BigDeck.clownDruid,deck2=BigDeck.bigWarrior,  gameNumber=100, debugLog=True)
+	a,b,c = play_set_of_games(Vector1, Vector2, deck1=BigDeck.clownDruid,deck2=BigDeck.bigWarrior,  gameNumber=1000, debugLog=True)
 	#a,b,c = play_set_of_games(Human1, Human2, deck1=[], deck2=[],gameNumber=1, debugLog=True,)# P1MAXMANA=10, P2MAXMANA=10)
 	#print("%d:%d"%(a,b))
 	#winsound.Beep(884, 2000)
