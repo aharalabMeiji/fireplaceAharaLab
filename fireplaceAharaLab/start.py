1a183b3e447cffd189c6e03c1627893414630e46
Kazushi Ahara <ahara@meiji.ac.jp>
2022-01-03 07:52:12 +0900

    HWをはじめてみた

--- fireplaceAharaLab/start.py
+++ fireplaceAharaLab/start.py
@@ -24,7 +24,7 @@ def main():
 	#ベクトルプレーヤー。意外と強い。このプレーヤーとサシで勝負して勝てるくらいが一応の目安。
 	Vector1=StandardVectorAgent("Vector1",StandardVectorAgent.StandardStep1\
 		,myOption=[3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8]\
-		,myClass=CardClass.DRUID)
+		,myClass=CardClass.HUNTER)
 		#,mulliganStrategy=StandardVectorAgent.StandardMulligan) 
 	Vector2=StandardVectorAgent("Vector2",StandardVectorAgent.StandardStep1\
 		,myOption=[3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8]\
@@ -59,7 +59,7 @@ def main():
 	#ゲームプレイ(きまったゲーム数を対戦し、勝ち数を数える)
 	#from utils import BigDeck
 	##BigDeck.faceHunter, BigDeck.clownDruid, BigDeck.bigWarrior
-	a,b,c = play_set_of_games(Vector1, Vector2, deck1=BigDeck.clownDruid,deck2=BigDeck.bigWarrior,  gameNumber=1000, debugLog=True)
+	a,b,c = play_set_of_games(Vector1, Vector2, deck1=BigDeck.faceHunter,deck2=BigDeck.bigWarrior,  gameNumber=3000, debugLog=True)
 	#a,b,c = play_set_of_games(Human1, Human2, deck1=[], deck2=[],gameNumber=1, debugLog=True,)# P1MAXMANA=10, P2MAXMANA=10)
 	#print("%d:%d"%(a,b))
 	#winsound.Beep(884, 2000)
