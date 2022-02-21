6f73334388a6db41805ec74238ad18e12d2151dd
Kazushi Ahara <ahara@meiji.ac.jp>
2022-02-22 00:40:29 +0900

    MLとつないでみた。

--- fireplaceAharaLab/agent_HappyCat.py
+++ fireplaceAharaLab/agent_HappyCat.py
@@ -10,6 +10,10 @@ from fireplace.game import Game
 from itertools import chain
 from agent_Standard import StandardVectorAgent, adjust_text_by_spellpower
 
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
 class HappyCat:
 	dict={}
 	def add_dict(id1, id2, phase):
@@ -26,6 +30,15 @@ class HappyCatAgent(Agent):
 		self.Vector=StandardVectorAgent("Vector",StandardVectorAgent.StandardStep1\
 		,myOption=[3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8]\
 		,myClass=CardClass.DRUID)
+		self.MLmodel2 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn2')
+		self.MLmodel3 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn3')
+		self.MLmodel4 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn4')
+		self.MLmodel5 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn5')
+		self.MLmodel6 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn6')
+		self.MLmodel7 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn7')
+		self.MLmodel8 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn8')
+		self.MLmodel9 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn9')
+		self.MLmodel10 = keras.models.load_model('fireplaceAharaLab/happyCatData/DWW_model_turn10')
 	def HappyCatAI(self, game, option=[], gameLog=[], debugLog=False):
 		self.player = game.current_player
 		self.game = game
@@ -236,7 +249,7 @@ class HappyCatAgent(Agent):
 			myCard = myChoice.card
 			if myChoice.card2!=None:
 				myCard=myChoice.card2
-			print("%s"%myCard, end='  ')
+			print("%s(%s)"%(myCard, myCard.id), end='  ')
 			if myCard.data.type==CardType.SPELL:#show spell in hand
 				print('<%2d> %s'%(\
 					myCard.cost, \
@@ -263,6 +276,35 @@ class HappyCatAgent(Agent):
 			pass
 		pass
 
+	def getBest3(self, prob):
+		n1=-1
+		n2=-1
+		n3=-1
+		v1=0.0
+		v2=0.0
+		v3=0.0
+		leng=len(prob)
+		for n in range(leng):
+			v=prob[n]
+			if v1<=v:
+				n3=n2
+				v3=v2
+				n2=n1
+				v2=v1
+				n1=n
+				v1=v
+			elif v2<=v:
+				n3=n2
+				v3=v2
+				n2=n
+				v2=v
+			elif v3<=v:
+				n3=n
+				v3=v
+				pass
+			pass
+		return [n1,n2,n3],[v1,v2,v3]
+
 	def ShowAnalysis(self, game, candidate):
 		##analysis by vector
 		v1,v2,v3=self.VectorChoice(candidate)
@@ -270,7 +312,38 @@ class HappyCatAgent(Agent):
 		print("%s(%d)"%(v1[1],v1[0]))
 		print("%s(%d)"%(v2[1],v2[0]))
 		print("%s(%d)"%(v3[1],v3[0]))
+		svec = self.getStatusVector(game)
+		#print(game.turn)
 		##analysis by DW_W_ML
+		pred=[]
+		another=['MINION','HERO','HEROPOWER','OTHERS','TURNEND']
+		if self.player.max_mana == 2:
+			pred = self.MLmodel2.predict([svec])
+		elif self.player.max_mana == 3:
+			pred = self.MLmodel3.predict([svec])
+		elif self.player.max_mana == 4:
+			pred = self.MLmodel4.predict([svec])
+		elif self.player.max_mana == 5:
+			pred = self.MLmodel5.predict([svec])
+		elif self.player.max_mana == 6:
+			pred = self.MLmodel6.predict([svec])
+		elif self.player.max_mana == 7:
+			pred = self.MLmodel7.predict([svec])
+		elif self.player.max_mana == 8:
+			pred = self.MLmodel8.predict([svec])
+		elif self.player.max_mana == 9:
+			pred = self.MLmodel9.predict([svec])
+		elif self.player.max_mana >= 10:
+			pred = self.MLmodel10.predict([svec])
+		if len(pred)>0:
+			pred = self.getBest3(pred[0])
+			leng = len(self.clownDruidCard)
+			for i in range(3):
+				if pred[0][i]<leng:
+					print("MLwin: %s:%f"%(self.clownDruidCard[pred[0][i]],pred[1][i]))
+				else:
+					print("MLwin: %s:%f"%(another[pred[0][i]-leng],pred[1][i]))
+			print(self.getBest3(pred[0]))
 		##analysis by DW_L_ML
 		pass
 	def InputByHand(self, game, myCandidate):
@@ -408,9 +481,23 @@ class HappyCatAgent(Agent):
 		else:
 			myMana=Manas[11]
 		return myMMana+myMana
+	#def vectorize(self,value,level,step):
+	#	w=[]
+	#	for i in range(level+1):
+	#		w.append(0)
+	#	t=int(value/step)
+	#	if t>level:
+	#		t=level
+	#	w[t]=1
+	#	return w
 	def heroes(self,my,his):
-		myHeroH = my.hero.health
-		hisHeroH = his.hero.health
+		#myHeroH = self.vectorize(my.hero.health+my.hero.armor,5,6)
+		#hisHeroH = self.vectorize(his.hero.health+his.hero.armor,5,6)
+		#myHeroA = self.vectorize(my.hero.atk+1,3,2)
+		#hisHeroA = self.vectorize(his.hero.atk+1,3,2)
+		#return myHeroH+myHeroA+hisHeroH+hisHeroA
+		myHeroH = my.hero.health+my.hero.armor
+		hisHeroH = his.hero.health+his.hero.armor
 		myHeroA = my.hero.atk
 		hisHeroA = his.hero.atk
 		return [myHeroH,myHeroA,hisHeroH,hisHeroA]
@@ -427,10 +514,17 @@ class HappyCatAgent(Agent):
 			if minion.taunt:
 				myTauntCharH += minion.health
 				myTauntCharA += minion.atk
-			if minion.devine_shield:
+			if minion.divine_shield:
 				myShieldCharA += minion.atk
 			if minion.reborn:
 				myRebornCharA += minion.atk
+		#myCharH = self.vectorize(myCharH+2,5,3)
+		#myCharA = self.vectorize(myCharA+2,5,3)
+		#myTauntCharH = self.vectorize(myTauntCharH+2,4,3)
+		#myTauntCharA = self.vectorize(myTauntCharA+1,4,2)
+		#myShieldCharA = self.vectorize(myShieldCharA+1,4,2)
+		#myRebornCharA = self.vectorize(myRebornCharA+1,4,2)
+		#return myCharH+myCharA+myTauntCharH+myTauntCharA+myShieldCharA+myRebornCharA
 		return [myCharH,myCharA,myTauntCharH,myTauntCharA,myShieldCharA,myRebornCharA]
 	def hisField(self, his):
 		hisCharH = 0
@@ -440,9 +534,14 @@ class HappyCatAgent(Agent):
 		for minion in his.field:
 			hisCharH += minion.health
 			hisCharA += minion.atk
-			if chminionar.taunt:
+			if minion.taunt:
 				hisTauntCharH += minion.health
-				hisTauntCharA += chminionar.atk
+				hisTauntCharA += minion.atk
+		#hisCharH = self.vectorize(hisCharH+2,5,3)
+		#hisCharA = self.vectorize(hisCharA+2,5,3)
+		#hisTauntCharH = self.vectorize(hisTauntCharH+2,4,3)
+		#hisTauntCharA = self.vectorize(hisTauntCharA+1,4,2)
+		#return hisCharH+hisCharA+hisTauntCharH+hisTauntCharA
 		return [hisCharH,hisCharA,hisTauntCharH,hisTauntCharA]
 	def myHand(self,my):
 		MinionCH = 0#手持ちのミニョンカードのHPの総和
@@ -462,31 +561,105 @@ class HappyCatAgent(Agent):
 				SpellCN += 1
 				if card.is_playable():
 					PlayableSpellCN += 1
+		#MinionCH=self.vectorize(MinionCH+2,5,3)
+		#PlayableMinionCH=self.vectorize(PlayableMinionCH+2,5,3)
+		#MinionCN=self.vectorize(MinionCN,5,1)
+		#PlayableMinionCN=self.vectorize(PlayableMinionCN,5,1)
+		#SpellCN=self.vectorize(SpellCN,5,1)
+		#PlayableSpellCN=self.vectorize(PlayableSpellCN,5,1)
+		#return MinionCH+MinionCN+PlayableMinionCH+PlayableMinionCN+SpellCN+PlayableSpellCN
 		return [MinionCH,MinionCN,PlayableMinionCH,PlayableMinionCN,SpellCN,PlayableSpellCN]
+	def myHandCard(self, my, myClass):
+		if myClass==CardClass.DRUID:
+			cardList=self.clownDruidCard
+		elif myClass==CardClass.WARRIOR:
+			cardList=self.bigWarriorCard
+		else:#CardClass.HUNTER
+			cardList=self.faceHunterCard
+		lenCardList=len(cardList)
+		handCardList=[0]*(lenCardList+3)
+		for card in my.hand:
+			another=True
+			for i in range(lenCardList):
+				if card.id==cardList[i]:
+					handCardList[i] = 1
+					another=False
+					break
+			if another:
+				if card.type==CardType.MINION:
+					handCardList[lenCardList] = 1
+				elif card.type==CardType.SPELL:
+					handCardList[lenCardList+1] = 1
+				else:
+					handCardList[lenCardList+2] = 1
+		return handCardList
+	def myFieldCard(self, my, myClass):
+		if myClass==CardClass.DRUID:
+			cardList=self.clownDruidCard
+		elif myClass==CardClass.WARRIOR:
+			cardList=self.bigWarriorCard
+		else:#CardClass.HUNTER
+			cardList=self.faceHunterCard
+		lenCardList=len(cardList)
+		fieldCardList=[0]*(lenCardList+3)
+		for card in my.field:
+			another=True
+			for i in range(lenCardList):
+				if not card.asleep and card.id==cardList[i]:
+					fieldCardList[i] = 1
+					another=False
+					break
+			if another and not card.asleep:
+				if card.type==CardType.MINION:
+					fieldCardList[lenCardList] = 1
+				elif card.type==CardType.SPELL:
+					fieldCardList[lenCardList+1] = 1
+				else:
+					fieldCardList[lenCardList+2] = 1
+		return fieldCardList
 	def getStatusVector(self, game):
 		my = game.current_player
 		his = game.current_player.opponent
-		return self.gameStatus(my)+self.heroes(my,his)+self.myField(my)+self.hisField(his)+self.myHand(my)
+		ret = self.gameStatus(my)\
+			+self.heroes(my,his)\
+			+self.myField(my)\
+			+self.hisField(his)\
+			+self.myHand(my)\
+			+self.myHandCard(my, my.hero.card_class)\
+			+self.myFieldCard(my, my.hero.card_class)
+		return ret
 
-	def getActionVector(self, candidate):
-		#druid
-		card=[]
+	def getActionVector(self, candidate, myClass):
+		card=-1
+		target=-1
 		another=True
-		for cardID in self.clownDruidCard:
-			if cardID==candidate.card.id:
-				card.append(1)
+		if myClass==CardClass.DRUID:
+			cardList=self.clownDruidCard
+		elif myClass==CardClass.WARRIOR:
+			cardList=self.bigWarriorCard
+		else:#CardClass.HUNTER
+			cardList=self.faceHunterCard
+		for i in range(len(cardList)):
+			cardID=cardList[i]
+			if candidate.card!=None and candidate.card!=None and cardID==candidate.card.id:
+				card=i
 				another=False
-			else:
-				card.append(0)
+				break
 		if another:
-			card.append(1)
+			if candidate.type ==ExceptionPlay.TURNEND:
+				card = len(cardList)+4
+			elif candidate.card.type==CardType.MINION:
+				card = len(cardList)
+			elif candidate.card.type==CardType.HERO:
+				card = len(cardList)+1
+			elif candidate.type==CardType.HERO_POWER:
+				card = len(cardList)+2
+			else: 
+				card = len(cardList)+3
+		if candidate.target==None:
+			target=0
+		elif candidate.target.type==CardType.HERO:
+			target=1
 		else:
-			card.append(0)
-		target=[]
-		if target==None:
-			target=[1,0,0]
-		elif target.type==CardType.HERO:
-			target=[0,1,0]
-		else:
-			target=[0,0,1]
-		return card+target
+			target=2
+		return card,target
