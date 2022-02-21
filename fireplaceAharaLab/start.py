d4849916b6088782f5548956c4e560b63de5aca5
takasho002 <rezerozero2@gmail.com>
2022-02-21 14:44:24 +0900

    https://scrapbox.io/rezerozero2-18614409/2022%E5%B9%B42%E6%9C%8821%E6%97%A5%E3%82%BC%E3%83%9F 実験結果追加

--- fireplaceAharaLab/start.py
+++ fireplaceAharaLab/start.py
@@ -66,10 +66,11 @@ def main():
 	#ゲームプレイ(きまったゲーム数を対戦し、勝ち数を数える)
 	#from utils import BigDeck
 	##BigDeck.faceHunter, BigDeck.clownDruid, BigDeck.bigWarrior
-	a,b,c = play_set_of_games(Random1, Random2, deck1=BigDeck.faceHunter , deck2=BigDeck.faceHunter, gameNumber=10000, debugLog=False,P1MAXMANA=3, P2MAXMANA=3)
+	a,b,c = play_set_of_games(Random1, Random2, deck1=BigDeck.faceHunter , deck2=BigDeck.faceHunter, gameNumber=10000, debugLog=False,P1MAXMANA=1, P2MAXMANA=4,P1HAND=2,P2HAND=3)
 	with open('output.csv', 'a', newline='') as csvfile:
 		writer = csv.writer(csvfile)
 		writer.writerow(['先手勝利数：',a])
+
 	#a,b,c = play_set_of_games(Human1, Human2, deck1=[], deck2=[],gameNumber=1, debugLog=True,)# P1MAXMANA=10, P2MAXMANA=10)
 	## 最初のプレイヤーからみてa勝ちb負けc分け。わけって何？？ｗ
 	#デッキを固定しての総当たり戦
