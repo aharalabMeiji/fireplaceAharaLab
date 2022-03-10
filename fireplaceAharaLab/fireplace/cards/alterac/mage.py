8d8457be1fc78acbac24fbb9c00b1b98ff23a698
ahara <ahara@meiji.ac.jp>
2022-03-10 16:46:04 +0900

    alterac_mageアプデ実装

--- fireplaceAharaLab/fireplace/cards/alterac/mage.py
+++ fireplaceAharaLab/fireplace/cards/alterac/mage.py
@@ -197,17 +197,45 @@ class AV_290:
 class ONY_006:# <4>[1626]
 	""" Deep Breath
 	Deal $@ damage to aminion and its neighbors.<i>(Improved by number of_other spells in your hand.)</i> """
-	#
+	requirements = {PlayReq.REQ_TARGET_TO_PLAY:0, PlayReq.REQ_MINION_TARGET:0, PlayReq.REQ_ENEMY_TARGET:0 }
+	def play(self):
+		target=self.target
+		controller=self.controller
+		amount=1
+		for card in controller.hand:
+			if card.type==CradType.SPELL:
+				amount+=1
+		self.script_data_text_0=amount# カード記載のため
+		Hit(target, amount).trigger(controller)
+		#instead of TARGET_ADJACENT
+		for i in range(len(controller.field)):
+			if controller.field[i]==target:
+				if i>0:
+					Hit(controller.field[i-1], amount).trigger(controller)
+				if i<len(controller.field)-1:
+					Hit(controller.field[i+1], amount).trigger(controller)
 	pass
 
 class ONY_007:# <4>[1626]
 	""" Haleh, Matron Protectorate
 	After you cast a spell, deal 4 damage randomly split among all enemies. """
-	#
+	events = OWN_SPELL_PLAY.on(Hit(RANDOM_ENEMY_CHARACTER,1) * 4)
 	pass
 
+class ONY_029_Choice(Choice):
+	def choose(self, card):
+		self.source._sidequest_counter_ += 1
+		if self.source._sidequest_counter_>=2:
+			self.next_choice=None
+		else:
+			self.next_choice=self
+		super().choose(card)
+		card.zone = Zone.PLAY
+		cards = self._args[1]
+		if isinstance(cards, LazyValue):
+			self.cards = cards.evaluate(self.source)
 class ONY_029:# <4>[1626]
 	""" Drakefire Amulet
 	[Tradeable][Discover] 2 Dragons. Summon them. """
-	#
+	play = 	ONY_029_Choice(CONTROLLER, RandomMinion(race=Race.DRAGON)*3)
 	pass
