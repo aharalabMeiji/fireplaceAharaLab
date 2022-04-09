9c416e98067d656666f624039a9f708e875bb6cb
Kazushi Ahara <ahara@meiji.ac.jp>
2022-04-09 23:53:09 +0900

    Update BG_minion_mecha.py

--- fireplaceAharaLab/fireplace/cards/battlegrounds/BG_minion_mecha.py
+++ fireplaceAharaLab/fireplace/cards/battlegrounds/BG_minion_mecha.py
@@ -1,19 +1,62 @@
 
 from ..utils import *
 
-#CardSet.BATTLEGROUNDS_Race.MECHANICAL=['BG20_401','BG20_401_G','BG21_022','BG21_022_G','BG21_023','BG21_023_G','BG21_024','BG21_024_G','BG21_025','BG21_025_G','BG21_HERO_030_Buddy','BG21_HERO_030_Buddy_G','BGS_006','BGS_023','BGS_024','BGS_027','BGS_028','BGS_071','TB_BaconShop_HERO_17_Buddy','TB_BaconShop_HERO_17_Buddy_G','TB_BaconShop_HERO_28_Buddy','TB_BaconShop_HERO_28_Buddy_G','TB_BaconUps_002','TB_BaconUps_002t','TB_BaconUps_006','TB_BaconUps_006t','TB_BaconUps_028','TB_BaconUps_032','TB_BaconUps_032t','TB_BaconUps_035','TB_BaconUps_039','TB_BaconUps_039t','TB_BaconUps_040','TB_BaconUps_040t','TB_BaconUps_041','TB_BaconUps_041t','TB_BaconUps_044','TB_BaconUps_046','TB_BaconUps_050','TB_BaconUps_063','TB_BaconUps_066','TB_BaconUps_069','TB_BaconUps_077','TB_BaconUps_080','TB_BaconUps_094','TB_BaconUps_099','TB_BaconUps_123','TB_BaconUps_153','TB_BaconUps_250',]
-class BG20_401:# <12>[1453]
-	""" Holy Mecherel
-	After another friendly minion loses [Divine Shield], gain [Divine Shield]. """
+BG_Minion_Mecha =[
+	'ULD_217', 'ULD_217e','TB_BaconUps_250',	#Micro Mummy(1)
+	'BG21_022','BG21_022_G',	#Pupbot(1)
+	'EX1_556', 'skele21','TB_BaconUps_006','TB_BaconUps_006t',	#Harvest Golem(2)
+	'BOT_606', 'TB_BaconUps_028',	#Kaboom Bot(2)
+	'GVG_048','TB_BaconUps_066',	#Metaltooth Leaper(2)
+	'BGS_071', 'TB_BaconUps_123',	#Deflect-o-Bot(3)
+	'BOT_312', 'BOT_312e','TB_BaconUps_032','TB_BaconUps_032t',	#Replicating Menace(3)
+	'GVG_055', 'TB_BaconUps_069',	#Screwjank Clunker(3)
+	'BOT_911', 'TB_BaconUps_099',	#Annoy-o-Module(4)
+	'BG21_024', 'BG21_024_G',	#Grease Bot(4)
+	'BOT_537', 'BOT_537t','TB_BaconUps_039', 'TB_BaconUps_039t',	#Mechano-Eg'g(4)
+	'BG21_023', 'BG21_023_G',	#Mechano-Tank(4)
+	'BG20_401', 'BG20_401_G',	#Holy Mecherel(5)
+	'GVG_113', 'TB_BaconUps_153',	#Foe Reaper 4000(6)
+	'BG21_025',	'BG21_025_G',	#Omega Buster(6)
+	]
+BG_PoolSet_Mecha=[
+	['ULD_217', 'BG21_022',],
+	['EX1_556', 'BOT_606', 'GVG_048', ],
+	['BGS_071', 'BOT_312', 'GVG_055', ],
+	['BOT_911', 'BG21_024', 'BOT_537', 'BG21_023', ],
+	['BG20_401', ],
+	['GVG_113', 'BG21_025', ],
+	]
+BG_Mecha_Gold={
+	'ULD_217':'TB_BaconUps_250',	#Micro Mummy(1)
+	'BG21_022':'BG21_022_G',	#Pupbot(1)
+	'EX1_556':'TB_BaconUps_006',	#Harvest Golem(2)
+	'BOT_606':'TB_BaconUps_028',	#Kaboom Bot(2)
+	'GVG_048':'TB_BaconUps_066',	#Metaltooth Leaper(2)
+	'BGS_071':'TB_BaconUps_123',	#Deflect-o-Bot(3)
+	'BOT_312':'TB_BaconUps_032',	#Replicating Menace(3)
+	'GVG_055':'TB_BaconUps_069',	#Screwjank Clunker(3)
+	'BOT_911':'TB_BaconUps_099',	#Annoy-o-Module(4)
+	'BG21_024':'BG21_024_G',	#Grease Bot(4)
+	'BOT_537':'TB_BaconUps_039',	#Mechano-Eg'g(4)
+	'BG21_023':'BG21_023_G',	#Mechano-Tank(4)
+	'BG20_401':'BG20_401_G',	#Holy Mecherel(5)
+	'GVG_113':'TB_BaconUps_153',	#Foe Reaper 4000(6)
+	'BG21_025':'BG21_025_G',	#Omega Buster(6)
+	}
+
+#Micro Mummy(1)
+class ULD_217:
+	"""
+	&lt;b&gt;Reborn&lt;/b&gt;At the end of your turn, giveanother random friendlyminion +1 Attack."""
+	pass
+ULD_217e=buff(1,0)
+class TB_BaconUps_250:# <5>[1453]
+	""" Micro Mummy
+	[Reborn]At the end of your turn, giveanother random friendlyminion +2 Attack. """
 	#
 	pass
 
-class BG20_401_G:# <12>[1453]
-	""" Holy Mecherel
-	After another friendly minion loses [Divine Shield], gain [Divine Shield]. """
-	#
-	pass
-
+#Pupbot(1)
 class BG21_022:# <12>[1453]
 	""" Pupbot
 	[Divine Shield] """
@@ -26,114 +69,14 @@ class BG21_022_G:# <12>[1453]
 	#
 	pass
 
-class BG21_023:# <12>[1453]
-	""" Mechano-Tank
-	[Avenge (2):] Deal 5 damage to the highest Health enemy minion. """
-	#
+#Harvest Golem(2)
+class EX1_556:
+	"""
+	&lt;b&gt;Deathrattle:&lt;/b&gt; Summon a 2/1 Damaged Golem."""
 	pass
-
-class BG21_023_G:# <12>[1453]
-	""" Mechano-Tank
-	[Avenge (2):] Deal 5 damageto the highest Healthenemy minion twice. """
-	#
-	pass
-
-class BG21_024:# <12>[1453]
-	""" Grease Bot
-	After a friendly minion loses [Divine Shield], give it +1/+1_permanently. """
-	#
-	pass
-
-class BG21_024_G:# <12>[1453]
-	""" Grease Bot
-	After a friendly minion loses [Divine Shield], give it +2/+2_permanently. """
-	#
-	pass
-
-class BG21_025:# <12>[1453]
-	""" Omega Buster
-	[Deathrattle:] Summon five 1/1 Microbots. For each that doesn't fit, give your Mechs +1/+1. """
-	#
-	pass
-
-class BG21_025_G:# <12>[1453]
-	""" Omega Buster
-	[Deathrattle:] Summon five 2/2 Microbots. For each that doesn't fit, give your Mechs +2/+2. """
-	#
-	pass
-
-class BGS_006:# <12>[1453]
-	""" Sneed's Old Shredder
-	[Deathrattle:] Summon a random [Legendary] minion. """
-	#
-	pass
-
-class BGS_023:# <12>[1453]
-	""" Piloted Shredder
-	[Deathrattle:] Summon a random 2-Cost minion. """
-	#
-	pass
-
-class BGS_024:# <12>[1453]
-	""" Piloted Sky Golem
-	[Deathrattle:] Summon a random 4-Cost minion. """
-	#
-	pass
-
-class BGS_027:# <12>[1453]
-	""" Micro Machine
-	At the start of each turn, gain +1 Attack. """
-	#
-	pass
-
-class BGS_028:# <7>[1453]
-	""" Pogo-Hopper
-	[Battlecry:] Gain +2/+2 foreach other Pogo-Hopperyou played this game. """
-	#
-	pass
-
-class BGS_071:# <12>[1453]
-	""" Deflect-o-Bot
-	[Divine Shield]Whenever you summon a Mechduring combat, gain +2 Attackand [Divine Shield]. """
-	#
-	pass
-
-class TB_BaconShop_HERO_17_Buddy:# <12>[1453]
-	""" Elementium Squirrel Bomb
-	[Deathrattle:] Deal 3 damageto a random enemy minionfor each of your Mechs thatdied this combat. """
-	#
-	pass
-
-class TB_BaconShop_HERO_17_Buddy_G:# <12>[1453]
-	""" Elementium Squirrel Bomb
-	[Deathrattle:] Deal 6 damageto a random enemy minionfor each of your Mechs thatdied this combat. """
-	#
-	pass
-
-class TB_BaconShop_HERO_28_Buddy:# <12>[1453]
-	""" Clockwork Assistant
-	[Battlecry:] [Discover] a minion from a higher Tavern Tier. """
-	#
-	pass
-
-class TB_BaconShop_HERO_28_Buddy_G:# <12>[1453]
-	""" Clockwork Assistant
-	[Battlecry:] [Discover] two minions from a higher Tavern Tier. """
-	#
-	pass
-
-class TB_BaconUps_002:# <12>[1453]
-	""" Mecharoo
-	[Deathrattle:] Summon a 2/2 Jo-E Bot. """
-	#
-	pass
-
-class TB_BaconUps_002t:# <12>[1453]
-	""" Jo-E Bot
-	 """
-	#
-	pass
-
+class skele21:
+	"""
+	"""
 class TB_BaconUps_006:# <12>[1453]
 	""" Harvest Golem
 	[Deathrattle:] Summon a 4/2 Damaged Golem. """
@@ -146,12 +89,47 @@ class TB_BaconUps_006t:# <12>[1453]
 	#
 	pass
 
+#Kaboom Bot(2)
+class BOT_606:
+	"""
+	&lt;b&gt;Deathrattle:&lt;/b&gt; Deal 4_damage to a random enemy minion. """
+	pass
 class TB_BaconUps_028:# <12>[1453]
 	""" Kaboom Bot
 	[Deathrattle:] Deal 4_damage to a random enemy minion twice. """
 	#
 	pass
 
+#Metaltooth Leaper(2)
+class GVG_048:
+	"""
+	&lt;b&gt;Battlecry:&lt;/b&gt; Give your other Mechs +2 Attack."""
+class TB_BaconUps_066:# <3>[1453]
+	""" Metaltooth Leaper
+	[Battlecry:] Give your other Mechs +4 Attack. """
+	#
+	pass
+
+#Deflect-o-Bot(3)
+class BGS_071:# <12>[1453]
+	""" Deflect-o-Bot
+	[Divine Shield]Whenever you summon a Mechduring combat, gain +2 Attackand [Divine Shield]. """
+	#
+	pass
+class TB_BaconUps_123:# <12>[1453]
+	""" Deflect-o-Bot
+	[Divine Shield]Whenever you summon a Mechduring combat, gain +4 Attackand [Divine Shield]. """
+	#
+	pass
+
+#Replicating Menace(3)
+class BOT_312:
+	"""Replicating Menace
+	&lt;b&gt;Magnetic&lt;/b&gt;&lt;b&gt;Deathrattle:&lt;/b&gt; Summon three 1/1 Microbots."""
+class BOT_312e:
+	"""Replicating Menace
+	"""
+	pass
 class TB_BaconUps_032:# <12>[1453]
 	""" Replicating Menace
 	[Magnetic][Deathrattle:] Summon three 2/2 Microbots. """
@@ -164,122 +142,108 @@ class TB_BaconUps_032t:# <12>[1453]
 	#
 	pass
 
-class TB_BaconUps_035:# <12>[1453]
-	""" Piloted Shredder
-	[Deathrattle:] Summon two random 2-Cost minions. """
-	#
+#Screwjank Clunker(3)
+class GVG_055:
+	""" Screwjank Clunker
+	&lt;b&gt;Battlecry:&lt;/b&gt; Give a friendly Mech +2/+2 """
 	pass
-
-class TB_BaconUps_039:# <5>[1453]
-	""" Mechano-Egg
-	[Deathrattle:] Summon a 16/16 Robosaur. """
-	#
-	pass
-
-class TB_BaconUps_039t:# <5>[1453]
-	""" Robosaur
-	 """
-	#
-	pass
-
-class TB_BaconUps_040:# <12>[1453]
-	""" Safeguard
-	[Taunt][Deathrattle:] Summon a 0/10Vault Safe with [Taunt]. """
-	#
-	pass
-
-class TB_BaconUps_040t:# <12>[1453]
-	""" Vault Safe
-	[Taunt] """
-	#
-	pass
-
-class TB_BaconUps_041:# <10>[1453]
-	""" Security Rover
-	Whenever this miniontakes damage, summon a4/6 Mech with [Taunt]. """
-	#
-	pass
-
-class TB_BaconUps_041t:# <10>[1453]
-	""" Guard Bot
-	[Taunt] """
-	#
-	pass
-
-class TB_BaconUps_044:# <7>[1453]
-	""" Iron Sensei
-	At the end of your turn, give another friendly Mech +4/+4. """
-	#
-	pass
-
-class TB_BaconUps_046:# <12>[1453]
-	""" Junkbot
-	Whenever a friendly Mech dies, gain +4/+4. """
-	#
-	pass
-
-class TB_BaconUps_050:# <12>[1453]
-	""" Piloted Sky Golem
-	[Deathrattle:] Summon two random 4-Cost minions. """
-	#
-	pass
-
-class TB_BaconUps_063:# <12>[1453]
-	""" Zoobot
-	[Battlecry:] Give a random friendly Beast, Dragon, and Murloc +2/+2. """
-	#
-	pass
-
-class TB_BaconUps_066:# <3>[1453]
-	""" Metaltooth Leaper
-	[Battlecry:] Give your other Mechs +4 Attack. """
-	#
-	pass
-
 class TB_BaconUps_069:# <10>[1453]
 	""" Screwjank Clunker
 	[Battlecry:] Give a friendly Mech +4/+4. """
 	#
 	pass
 
-class TB_BaconUps_077:# <7>[1453]
-	""" Pogo-Hopper
-	[Battlecry:] Gain +4/+4 foreach other Pogo-Hopperyou played this game. """
-	#
+#Annoy-o-Module(4)
+class BOT_911:
+	"""
+	&lt;b&gt;Magnetic&lt;/b&gt;&lt;b&gt;Divine Shield&lt;/b&gt;&lt;b&gt;Taunt&lt;/b&gt;"""
 	pass
-
-class TB_BaconUps_080:# <12>[1453]
-	""" Sneed's Old Shredder
-	[Deathrattle:] Summon 2 random [Legendary] minions. """
-	#
-	pass
-
-class TB_BaconUps_094:# <12>[1453]
-	""" Micro Machine
-	At the start of each turn, gain +2 Attack. """
-	#
-	pass
-
 class TB_BaconUps_099:# <5>[1453]
 	""" Annoy-o-Module
 	[Magnetic][Divine Shield][Taunt] """
 	#
 	pass
 
-class TB_BaconUps_123:# <12>[1453]
-	""" Deflect-o-Bot
-	[Divine Shield]Whenever you summon a Mechduring combat, gain +4 Attackand [Divine Shield]. """
+#Grease Bot(4)
+class BG21_024:# <12>[1453]
+	""" Grease Bot
+	After a friendly minion loses [Divine Shield], give it +1/+1_permanently. """
 	#
 	pass
 
+class BG21_024_G:# <12>[1453]
+	""" Grease Bot
+	After a friendly minion loses [Divine Shield], give it +2/+2_permanently. """
+	#
+	pass
+
+#Mechano-Egg(4)
+class BOT_537:
+	"""
+	&lt;b&gt;Deathrattle:&lt;/b&gt; Summon an 8/8 Robosaur."""
+	pass
+class BOT_537t:
+	"""
+	"""
+	pass
+class TB_BaconUps_039:# <5>[1453]
+	""" Mechano-Egg
+	[Deathrattle:] Summon a 16/16 Robosaur. """
+	#
+	pass
+class TB_BaconUps_039t:
+	""" Robosaur
+	"""
+#Mechano-Tank(4)
+class BG21_023:# <12>[1453]
+	""" Mechano-Tank
+	[Avenge (2):] Deal 5 damage to the highest Health enemy minion. """
+	#
+	pass
+
+class BG21_023_G:# <12>[1453]
+	""" Mechano-Tank
+	[Avenge (2):] Deal 5 damageto the highest Healthenemy minion twice. """
+	#
+	pass
+
+#Holy Mecherel(5)
+class BG20_401:# <12>[1453]
+	""" Holy Mecherel
+	After another friendly minion loses [Divine Shield], gain [Divine Shield]. """
+	#
+	pass
+
+class BG20_401_G:# <12>[1453]
+	""" Holy Mecherel
+	After another friendly minion loses [Divine Shield], gain [Divine Shield]. """
+	#
+	pass
+
+#Foe Reaper 4000(6)
+class GVG_113:
+	""" Foe Reaper 4000
+	Also damages the minions next to whomever it attacks. """
+	pass
 class TB_BaconUps_153:# <12>[1453]
 	""" Foe Reaper 4000
 	Also damages the minions next to whomever it attacks. """
 	#
 	pass
 
-class TB_BaconUps_250:# <5>[1453]
-	""" Micro Mummy
-	[Reborn]At the end of your turn, giveanother random friendlyminion +2 Attack. """
+#Omega Buster(6)
+class BG21_025:# <12>[1453]
+	""" Omega Buster
+	[Deathrattle:] Summon five 1/1 Microbots. For each that doesn't fit, give your Mechs +1/+1. """
 	#
 	pass
+
+class BG21_025_G:# <12>[1453]
+	""" Omega Buster
+	[Deathrattle:] Summon five 2/2 Microbots. For each that doesn't fit, give your Mechs +2/+2. """
+	#
+	pass
+
+
+
+
