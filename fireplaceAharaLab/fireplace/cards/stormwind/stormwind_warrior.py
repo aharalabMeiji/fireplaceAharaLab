44e6e9771b0ac4bcc3359b36db4cda598ee09c5c
Kazushi Ahara <ahara@meiji.ac.jp>
2022-08-12 07:30:22 +0900

    StormWind_Warrior

--- fireplaceAharaLab/fireplace/cards/stormwind/stormwind_warrior.py
+++ fireplaceAharaLab/fireplace/cards/stormwind/stormwind_warrior.py
@@ -1,353 +1,31 @@
 
 from ..utils import *
 
-
 ################################################
 
 from ..utils import *
 
 StormWind_Warrior=[]
-StormWind_Cornelius_Roame=True
-StormWind_Stalwart=True
-StormWind_Rokara=True
-StormWind_For_the_Frostwolves=True
-StormWind_Rokara=True
-StormWind_Cornelius_Roame=True
-StormWind_Armor_Up_Up_Up=True
-StormWind_A_Twisted_Monstrosity=True
-StormWind_Monstrous_Power=True
-StormWind_Forsaken_Might=True
-StormWind_Hogger=True
-StormWind_Mr_Smite=True
-StormWind_Rokara=True
-StormWind_Rokara=True
-StormWind_Rokara=True
-StormWind_Man_the_Cannons=True
-StormWind_Defias_Cannoneer=True
-StormWind_Blacksmithing_Hammer=True
-StormWind_Blacksmithing=True
-StormWind_Remote_Controlled_Golem=True
-StormWind_Cowardly_Grunt=True
-StormWind_Provoke=True
-StormWind_Lothar=True
-StormWind_Shiver_Their_Timbers=True
-StormWind_Raid_the_Docks=True
-StormWind_Create_a_Distraction=True
-StormWind_Secure_the_Supplies=True
-StormWind_Capn_Rokara=True
-StormWind_The_Juggernaut=True
-StormWind_Harbor_Scamp=True
-StormWind_Cargo_Guard=True
-StormWind_Stormwind_Freebooter=True
-StormWind_Heavy_Plate=True
-StormWind_Remote_Controlled_Golem=True
-StormWind_Golem_Parts=True
-StormWind_Rokara=True
 
+StormWind_Man_the_Cannons=True  ###
+StormWind_Defias_Cannoneer=True  ###
+StormWind_Blacksmithing_Hammer=True  ###
+StormWind_Cowardly_Grunt=True  ###
+StormWind_Provoke=True  ###
+StormWind_Lothar=True  ###
+StormWind_Shiver_Their_Timbers=True  ###
+StormWind_Raid_the_Docks=True  ###
+StormWind_Harbor_Scamp=True  ###
+StormWind_Cargo_Guard=True  ###
+StormWind_Stormwind_Freebooter=True  ###
+StormWind_Heavy_Plate=True  ###
+StormWind_Remote_Controlled_Golem=True  ###
 
+#########################################################
 
-if StormWind_Cornelius_Roame:# 
-	StormWind_Warrior+=['BOM_05_Cornelius_008hb']
-class BOM_05_Cornelius_008hb:# <10>[1578]
-	""" Cornelius Roame
-	Through the flames, Tamsin sees a lone soldier approach. It is her father, with armor impenetrable. """
-	#
-	pass
-
-if StormWind_Stalwart:# 
-	StormWind_Warrior+=['BOM_05_Cornelius_008p']
-class BOM_05_Cornelius_008p:# <10>[1578]
-	""" Stalwart
-	[Passive Hero Power]Damage dealt to youby minions is reducedto zero. """
-	#
-	pass
-
-if StormWind_Rokara:# 
-	StormWind_Warrior+=['BOM_05_Rokara_004hb']
-class BOM_05_Rokara_004hb:# <10>[1578]
-	""" Rokara
-	 """
-	#
-	pass
-
-if StormWind_For_the_Frostwolves:# 
-	StormWind_Warrior+=['BOM_05_Rokara_004p']
-class BOM_05_Rokara_004p:# <10>[1578]
-	""" For the Frostwolves!
-	[Hero Power]Gain 1 Armor for eachminion you control. """
-	#
-	pass
-
-if StormWind_Rokara:# 
-	StormWind_Warrior+=['BOM_05_Rokara_03t']
-class BOM_05_Rokara_03t:# <10>[1578]
-	""" Rokara
-	[Rush][Deathrattle:] Go [Dormant] for 2 turns. """
-	#
-	pass
-
-if StormWind_Cornelius_Roame:# 
-	StormWind_Warrior+=['BOM_06_Cornelius_003hb']
-class BOM_06_Cornelius_003hb:# <10>[1578]
-	""" Cornelius Roame
-	Cariel returns home to Stormwind unaware that Tamsin's attack on the city is imminent. """
-	#
-	pass
-
-if StormWind_Armor_Up_Up_Up:# 
-	StormWind_Warrior+=['BOM_06_Cornelius_003p']
-class BOM_06_Cornelius_003p:# <10>[1578]
-	""" Armor Up Up Up!
-	[Passive Hero Power]After an enemy minion is destroyed, gain 1 Armor. """
-	#
-	pass
-
-if StormWind_A_Twisted_Monstrosity:# 
-	StormWind_Warrior+=['BOM_06_Monstrosity_007hb']
-class BOM_06_Monstrosity_007hb:# <10>[1578]
-	""" A Twisted Monstrosity
-	Tamsin's cruel treatment of her father has resulted in his_current...condition. """
-	#
-	pass
-
-if StormWind_Monstrous_Power:# 
-	StormWind_Warrior+=['BOM_06_Monstrosity_007p']
-class BOM_06_Monstrosity_007p:# <10>[1578]
-	""" Monstrous Power
-	[Passive Hero Power]After an enemy minion dies, equip a Forsaken Arm. If you already have one, give it +1/+1. """
-	#
-	pass
-
-if StormWind_Forsaken_Might:# 
-	StormWind_Warrior+=['BOM_06_Monstrosity_007p1e1']
-class BOM_06_Monstrosity_007p1e1:# <10>[1578]
-	""" Forsaken Might
-	+1 Attack and +1 Durability. """
-	#
-	pass
-
-if StormWind_Hogger:# 
-	StormWind_Warrior+=['BOM_07_Scabbs_Hogger_003hb']
-class BOM_07_Scabbs_Hogger_003hb:# <10>[1578]
-	""" Hogger
-	<i>The Scourge of Elwynn leads a tough gang of convicts_in_the_Stockade.</i> """
-	#
-	pass
-
-if StormWind_Mr_Smite:# 
-	StormWind_Warrior+=['BOM_07_Scabbs_MrSmite_006hb']
-class BOM_07_Scabbs_MrSmite_006hb:# <10>[1578]
-	""" Mr. Smite
-	<i>The Defias plan to smugglethe shards of naaru outof Stormwind via pirate ship!</i> """
-	#
-	pass
-
-if StormWind_Rokara:# 
-	StormWind_Warrior+=['BOM_07_Scabbs_Rokara_006t']
-class BOM_07_Scabbs_Rokara_006t:# <10>[1578]
-	""" Rokara
-	[Taunt][Deathrattle]: Return thisto your hand. """
-	#
-	pass
-
-if StormWind_Rokara:# 
-	StormWind_Warrior+=['BOM_07_Scabbs_Rokara_007t']
-class BOM_07_Scabbs_Rokara_007t:# <10>[1578]
-	""" Rokara
-	[Taunt][Deathrattle]: Return thisto your hand. """
-	#
-	pass
-
-if StormWind_Rokara:# 
-	StormWind_Warrior+=['BOM_07_Scabbs_Rokara_008t']
-class BOM_07_Scabbs_Rokara_008t:# <10>[1578]
-	""" Rokara
-	[Taunt][Deathrattle]: Return thisto your hand. """
-	#
-	pass
 
 if StormWind_Man_the_Cannons:# 
 	StormWind_Warrior+=['DED_518']
-class DED_518:# <10>[1578]
-	""" Man the Cannons
-	Deal $3 damage to a minion and $1 damage to all other minions. """
-	#
-	pass
-
-if StormWind_Defias_Cannoneer:# 
-	StormWind_Warrior+=['DED_519']
-class DED_519:# <10>[1578]
-	""" Defias Cannoneer
-	After your hero attacks,deal 2 damage to arandom enemy twice. """
-	#
-	pass
-
-if StormWind_Blacksmithing_Hammer:# 
-	StormWind_Warrior+=['DED_527']
-class DED_527:# <10>[1578]
-	""" Blacksmithing Hammer
-	[Tradeable]After you [Trade] this,_gain +2 Durability. """
-	#
-	pass
-
-if StormWind_Blacksmithing:# 
-	StormWind_Warrior+=['DED_527e']
-class DED_527e:# <10>[1578]
-	""" Blacksmithing
-	+2 Durability. """
-	#
-	pass
-
-if StormWind_Remote_Controlled_Golem:# 
-	StormWind_Warrior+=['Story_11_RemoteControlPuzzle']
-class Story_11_RemoteControlPuzzle:# <10>[1578]
-	""" Remote-Controlled Golem
-	After this takes damage,shuffle two Golem Parts intoyour deck. When drawn,__summon a 2/1 Mech. """
-	#
-	pass
-
-if StormWind_Cowardly_Grunt:# 
-	StormWind_Warrior+=['SW_021']
-class SW_021:# <10>[1578]
-	""" Cowardly Grunt
-	[Deathrattle:] Summon a minion from your deck. """
-	#
-	pass
-
-if StormWind_Provoke:# 
-	StormWind_Warrior+=['SW_023']
-class SW_023:# <10>[1578]
-	""" Provoke
-	[Tradeable]Choose a friendly minion.Enemy minions attack it. """
-	#
-	pass
-
-if StormWind_Lothar:# 
-	StormWind_Warrior+=['SW_024']
-class SW_024:# <10>[1578]
-	""" Lothar
-	At the end of your turn, attack a random enemy minion. If it dies, gain +3/+3. """
-	#
-	pass
-
-if StormWind_Shiver_Their_Timbers:# 
-	StormWind_Warrior+=['SW_027']
-class SW_027:# <10>[1578]
-	""" Shiver Their Timbers!
-	Deal $2 damage to a minion. If you control a Pirate, deal $5 instead. """
-	#
-	pass
-
-if StormWind_Raid_the_Docks:# 
-	StormWind_Warrior+=['SW_028']
-class SW_028:# <10>[1578]
-	""" Raid the Docks
-	[Questline:] Play 3 Pirates.[Reward:] Draw a weapon. """
-	#
-	pass
-
-if StormWind_Create_a_Distraction:# 
-	StormWind_Warrior+=['SW_028t']
-class SW_028t:# <10>[1578]
-	""" Create a Distraction
-	[Questline:] Play 3 Pirates.[Reward:] Deal $2 damageto a random enemy twice. """
-	#
-	pass
-
-if StormWind_Secure_the_Supplies:# 
-	StormWind_Warrior+=['SW_028t2']
-class SW_028t2:# <10>[1578]
-	""" Secure the Supplies
-	[Questline:] Play 3 Pirates.[Reward:] Cap'n Rokara. """
-	#
-	pass
-
-if StormWind_Capn_Rokara:# 
-	StormWind_Warrior+=['SW_028t5']
-class SW_028t5:# <10>[1578]
-	""" Cap'n Rokara
-	[Battlecry:] Summon The Juggernaut! """
-	#
-	pass
-
-if StormWind_The_Juggernaut:# 
-	StormWind_Warrior+=['SW_028t6']
-class SW_028t6:# <10>[1578]
-	""" The Juggernaut
-	[Start of Your Turn:]Summon a Pirate, equip aWarrior weapon, and fire two cannons that deal 2 damage! """
-	#
-	pass
-
-if StormWind_Harbor_Scamp:# 
-	StormWind_Warrior+=['SW_029']
-class SW_029:# <10>[1578]
-	""" Harbor Scamp
-	[Battlecry:] Draw a Pirate. """
-	#
-	pass
-
-if StormWind_Cargo_Guard:# 
-	StormWind_Warrior+=['SW_030']
-class SW_030:# <10>[1578]
-	""" Cargo Guard
-	At the end of your turn, gain 3 Armor. """
-	#
-	pass
-
-if StormWind_Stormwind_Freebooter:# 
-	StormWind_Warrior+=['SW_093']
-class SW_093:# <10>[1578]
-	""" Stormwind Freebooter
-	[Battlecry:] Give your hero +2 Attack this turn. """
-	#
-	pass
-
-if StormWind_Heavy_Plate:# 
-	StormWind_Warrior+=['SW_094']
-class SW_094:# <10>[1578]
-	""" Heavy Plate
-	[Tradeable]Gain 8 Armor. """
-	#
-	pass
-
-if StormWind_Remote_Controlled_Golem:# 
-	StormWind_Warrior+=['SW_097']
-class SW_097:# <10>[1578]
-	""" Remote-Controlled Golem
-	After this takes damage,shuffle two Golem Parts intoyour deck. When drawn,__summon a 2/1 Mech. """
-	#
-	pass
-
-if StormWind_Golem_Parts:# 
-	StormWind_Warrior+=['SW_097t']
-class SW_097t:# <10>[1578]
-	""" Golem Parts
-	[Casts When Drawn]Summon a 2/1 Damaged Golem. """
-	#
-	pass
-
-if StormWind_Rokara:# 
-	StormWind_Warrior+=['TB_01_BOM_Mercs_Rokara_001h']
-class TB_01_BOM_Mercs_Rokara_001h:# <10>[1578]
-	""" Rokara
-	 """
-	#
-	pass
-
-
-
-
-
-
-
-
-
-##############################################
-
-
-
-## warrior
-
 class DED_518:# <10>[1578] ###OK
 	""" Man the Cannons
 	Deal $3 damage to a minion and $1 damage to all other minions. """
@@ -356,67 +34,95 @@ class DED_518:# <10>[1578] ###OK
 	#
 	pass
 
+
+
+
+if StormWind_Defias_Cannoneer:# 
+	StormWind_Warrior+=['DED_519']
 class DED_519:# <10>[1578] ###OK
 	""" Defias Cannoneer
 	After your hero attacks,deal 2 damage to a random enemy twice. """
 	events = Attack(FRIENDLY_HERO).on(Hit(RANDOM(ENEMY_CHARACTERS),2) * 2)
 	pass
 
+
+
+
+if StormWind_Blacksmithing_Hammer:# 
+	StormWind_Warrior+=['DED_527']
+	StormWind_Warrior+=['DED_527e']
 class DED_527:# <10>[1578] ###OK
 	""" Blacksmithing Hammer
 	[Tradeable]After you [Trade] this, _gain +2 Durability. """
 	# weapon 
 	#trade = Buff(SELF, 'DED_527e')
 	pass
-
 DED_527e = buff(0,2)# <10>[1578]
 """ Blacksmithing
 +2 Durability. """
 
 
 
-#class SW_021:###OK bigWarrior
-#	"""Cowardly Grunt
-#	Deathrattle: Summon a minion from your deck."""
-#	# CardDefsにdeathrattleタグがついていない
-#	play = SetTag(SELF, (GameTag.DEATHRATTLE, ))
-#	deathrattle = Summon(CONTROLLER, RANDOM(FRIENDLY_DECK+MINION))
-#	pass
 
-#class SW_023:###OK bigWarrior
-#	"""Provoke
-#	Tradeable: Choose a friendly minion. Enemy minions attack it."""
-#	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_MINIMUM_ENEMY_MINIONS: 1,
-#					PlayReq.REQ_TARGET_TO_PLAY: 0, PlayReq.REQ_FRIENDLY_TARGET: 0}
-#	def play(self):
-#		for target in self.targets:
-#			for minion in self.controller.opponent.field:
-#				Attack(minion,target).trigger(self.controller)
-#	pass
+if StormWind_Cowardly_Grunt:# 
+	StormWind_Warrior+=['SW_021']
+class SW_021:###OK bigWarrior
+	"""Cowardly Grunt
+	Deathrattle: Summon a minion from your deck."""
+	# CardDefsにdeathrattleタグがついていない
+	play = SetTag(SELF, (GameTag.DEATHRATTLE, ))
+	deathrattle = Summon(CONTROLLER, RANDOM(FRIENDLY_DECK+MINION))
+	pass
 
-#class SW_024_Action(TargetedAction):
-#	TARGET = ActionArg()# 
-#	def do(self, source, target):
-#		controller = source.controller
-#		enemy = controller.opponent
-#		if len(enemy.field)==0:
-#			return
-#		deffender = random.choice(enemy.field)
-#		Attack(source, deffender).trigger(controller)
-#		if deffender.health <= 0:
-#			Buff(source, 'SW_024e').trigger(controller)
-#		pass
 
-#class SW_024:###OK  bigWarrior
-#	"""Lothar
-#	At the end of your turn, attack a random enemy minion. If it dies, gain +3/+3."""
-#	events = OWN_TURN_END.on(SW_024_Action(SELF))
-#	#Attackは使い方が難しい。BAR_844のように、事象発生の条件として使われるほうがふつうなので。
-#	#events = OWN_TURN_END.on(Attack(SELF, RANDOM_ENEMY_MINION).then(
-#	#	Dead(Attack.DEFENDER) & Buff(SELF, "SW_024e")))
-#	pass
-#SW_024e = buff(atk=3, health=3)
 
+
+if StormWind_Provoke:# 
+	StormWind_Warrior+=['SW_023']
+class SW_023:###OK bigWarrior
+	"""Provoke
+	Tradeable: Choose a friendly minion. Enemy minions attack it."""
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_MINIMUM_ENEMY_MINIONS: 1,
+					PlayReq.REQ_TARGET_TO_PLAY: 0, PlayReq.REQ_FRIENDLY_TARGET: 0}
+	def play(self):
+		for target in self.targets:
+			for minion in self.controller.opponent.field:
+				Attack(minion,target).trigger(self.controller)
+	pass
+
+
+
+
+
+if StormWind_Lothar:# 
+	StormWind_Warrior+=['SW_024','SW_024e']
+class SW_024_Action(TargetedAction):
+	TARGET = ActionArg()# 
+	def do(self, source, target):
+		controller = source.controller
+		enemy = controller.opponent
+		if len(enemy.field)==0:
+			return
+		deffender = random.choice(enemy.field)
+		Attack(source, deffender).trigger(controller)
+		if deffender.health <= 0:
+			Buff(source, 'SW_024e').trigger(controller)
+		pass
+class SW_024:###OK  bigWarrior
+	"""Lothar
+	At the end of your turn, attack a random enemy minion. If it dies, gain +3/+3."""
+	events = OWN_TURN_END.on(SW_024_Action(SELF))
+	#Attackは使い方が難しい。BAR_844のように、事象発生の条件として使われるほうがふつうなので。
+	#events = OWN_TURN_END.on(Attack(SELF, RANDOM_ENEMY_MINION).then(
+	#	Dead(Attack.DEFENDER) & Buff(SELF, "SW_024e")))
+	pass
+SW_024e = buff(atk=3, health=3)
+
+
+
+
+if StormWind_Shiver_Their_Timbers:# 
+	StormWind_Warrior+=['SW_027']
 class SW_027:##OK <10>[1578]
 	""" Shiver Their Timbers!
 	Deal $2 damage to a minion. If you control a Pirate, deal $5 instead. """
@@ -425,6 +131,15 @@ class SW_027:##OK <10>[1578]
 	#
 	pass
 
+
+
+
+if StormWind_Raid_the_Docks:# 
+	StormWind_Warrior+=['SW_028']
+	StormWind_Warrior+=['SW_028t']
+	StormWind_Warrior+=['SW_028t2']
+	StormWind_Warrior+=['SW_028t5']
+	StormWind_Warrior+=['SW_028t6']
 class SW_028:##OK <10>[1578]
 	""" Raid the Docks
 	[Questline:] Play 3 Pirates.[Reward:] Draw a weapon. """
@@ -480,12 +195,22 @@ class SW_028t6:##OK <10>[1578]
 		)
 	pass
 
+
+
+
+if StormWind_Harbor_Scamp:# 
+	StormWind_Warrior+=['SW_029']
 class SW_029:##OK <10>[1578]
 	""" Harbor Scamp
 	[Battlecry:] Draw a Pirate. """
 	play = Give(CONTROLLER, RandomMinion(race = Race.PIRATE))
 	pass
 
+
+
+
+if StormWind_Cargo_Guard:# 
+	StormWind_Warrior+=['SW_030']
 class SW_030:##OK <10>[1578]
 	""" Cargo Guard
 	At the end of your turn, gain 3 Armor. """
@@ -494,6 +219,11 @@ class SW_030:##OK <10>[1578]
 		)
 	pass
 
+
+
+
+if StormWind_Stormwind_Freebooter:# 
+	StormWind_Warrior+=['SW_093','SW_093e']
 class SW_093:##OK <10>[1578]
 	""" Stormwind Freebooter
 	[Battlecry:] Give your hero +2 Attack this turn. """
@@ -501,12 +231,23 @@ class SW_093:##OK <10>[1578]
 	pass
 SW_093e = buff(atk=2 )# ONE_TURN_EFFECT
 
-#class SW_094:###OK
-#	"""Heavy Plate
-#	Tradeable: Gain 8 Armor."""
-#	play = GainArmor(FRIENDLY_HERO, 8)
-#	pass
 
+
+
+if StormWind_Heavy_Plate:# 
+	StormWind_Warrior+=['SW_094']
+class SW_094:###OK
+	"""Heavy Plate
+	Tradeable: Gain 8 Armor."""
+	play = GainArmor(FRIENDLY_HERO, 8)
+	pass
+
+
+
+
+if StormWind_Remote_Controlled_Golem:# 
+	StormWind_Warrior+=['SW_097']
+	StormWind_Warrior+=['SW_097t','skele21']
 class SW_097:# <10>[1578]
 	""" Remote-Controlled Golem
 	After this takes damage,shuffle two Golem Parts intoyour deck. When drawn,__summon a 2/1 Mech. """
@@ -521,3 +262,4 @@ class SW_097t:# <10>[1578]
 class skele21:
 	""" Damaged Golem """
 	pass
+
