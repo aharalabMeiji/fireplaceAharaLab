a67382619444c9d6ac53b1350b468e485dec639e
Kazushi Ahara <ahara@meiji.ac.jp>
2022-08-12 07:08:38 +0900

    StormWind_Rogue

--- fireplaceAharaLab/fireplace/cards/stormwind/stormwind_rogue.py
+++ fireplaceAharaLab/fireplace/cards/stormwind/stormwind_rogue.py
@@ -2,261 +2,44 @@
 from ..utils import *
 
 StormWind_Rogue=[]
-StormWind_A_Cheesemonger=True
-StormWind_Hooktusk=True
-StormWind_Dagger_Mastery=True
-StormWind_Pick_the_Lock=True
-StormWind_Bob_the_Bartender=True
-StormWind_Whats_Cooking?=True
-StormWind_Edwin_VanCleef=True
-StormWind_Dangerous_Defiant=True
-StormWind_Scabbs_Cutterbutter=True
-StormWind_Dagger_Mastery=True
-StormWind_Scabbs_Cutterbutter=True
-StormWind_Dagger_Mastery=True
-StormWind_Scabbs_Cutterbutter=True
-StormWind_Poisoned_Daggers=True
-StormWind_Scabbs_Cutterbutter=True
-StormWind_Poisoned_Daggers=True
-StormWind_Scabbs_Cutterbutter=True
-StormWind_Scabbs_Cutterbutter=True
-StormWind_Scabbs_Cutterbutter=True
-StormWind_Scabbs_Cutterbutter=True
-StormWind_Poisoned_Daggers=True
-StormWind_Sneed=True
-StormWind_Subtle_Butter=True
-StormWind_Blackwater_Cutlass=True
-StormWind_Blackwater_Treasure=True
-StormWind_Parrrley=True
-StormWind_Edwin,_Defias_Kingpin=True
-StormWind_Defias_Supplies=True
-StormWind_King_of_the_Brotherhood=True
-StormWind_Wardens_Determination=True
-StormWind_Determined=True
-StormWind_Wardens_Glaive=True
-StormWind_Parrrley=True
-StormWind_Maestra_of_the_Masquerade=True
-StormWind_Find_the_Imposter=True
-StormWind_Learn_the_Truth=True
-StormWind_Marked_a_Traitor=True
-StormWind_Spymaster_Scabbs=True
-StormWind_Fizzflash_Distractor=True
-StormWind_Distracted=True
-StormWind_Spy_o_matic=True
-StormWind_Noggen_Fog_Generator=True
-StormWind_Noggen_Fog=True
-StormWind_Hidden_Gyroblade=True
-StormWind_Undercover_Mole=True
-StormWind_Counterfeit_Blade=True
-StormWind_Counterfeit_Blade=True
-StormWind_Garrote=True
-StormWind_Bleed=True
-StormWind_Sketchy_Information=True
-StormWind_SI7_Informant=True
-StormWind_Well_Informed=True
-StormWind_SI7_Extortion=True
-StormWind_SI7_Operative=True
-StormWind_SI7_Assassin=True
-StormWind_Loan_Shark=True
-if StormWind_A_Cheesemonger:# 
-	StormWind_Rogue+=['BOM_05_EllingTrias_005hb']
-class BOM_05_EllingTrias_005hb:# <7>[1578]
-	""" A Cheesemonger
-	Undercover SI:7 agents areall over Stormwind, in placesyou would never suspect. """
-	#
-	pass
 
-if StormWind_Hooktusk:# 
-	StormWind_Rogue+=['BOM_05_Hooktusk_002hb']
-class BOM_05_Hooktusk_002hb:# <7>[1578]
-	""" Hooktusk
-	Booty Bay is quite close to Gurubashi Arena, don't you know. """
-	#
-	pass
+StormWind_Blackwater_Cutlass=True  ###
+StormWind_Parrrley=True  ###
+StormWind_Edwin_Defias_Kingpin=True  ###
+StormWind_Maestra_of_the_Masquerade=True  ###
+StormWind_Find_the_Imposter=True  ###
+StormWind_Counterfeit_Blade=True  ###
+StormWind_Garrote=True  ###
+StormWind_Sketchy_Information=True  ###
+StormWind_SI7_Informant=True  ###
+StormWind_SI7_Extortion=True  ###
+StormWind_SI7_Operative=True  ###
+StormWind_SI7_Assassin=True  ###
+StormWind_Loan_Shark=True  ###
 
-if StormWind_Dagger_Mastery:# 
-	StormWind_Rogue+=['BOM_06_Cariel_001p']
-class BOM_06_Cariel_001p:# <7>[1578]
-	""" Dagger Mastery
-	[Hero Power]Equip a 1/2 Dagger. """
-	#
-	pass
+###########################################
 
-if StormWind_Pick_the_Lock:# 
-	StormWind_Rogue+=['BOM_07_PickTheLock_004s']
-class BOM_07_PickTheLock_004s:# <7>[1578]
-	""" Pick the Lock
-	Pick a lock to helpyou escape. """
-	#
-	pass
 
-if StormWind_Bob_the_Bartender:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Bob_001hb']
-class BOM_07_Scabbs_Bob_001hb:# <7>[1578]
-	""" Bob the Bartender
-	<i>An open-eared publican is the ideal informant, and Scabbs's former_mentoris_the_best.</i> """
-	#
-	pass
 
-if StormWind_Whats_Cooking?:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Cookie_007p']
-class BOM_07_Scabbs_Cookie_007p:# <7>[1578]
-	""" What's Cooking?
-	[Passive Hero Power]Every three turns,Cookie ducks inside his cauldron for a turn. """
-	#
-	pass
 
-if StormWind_Edwin_VanCleef:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_EdwinVanCleef_008hb']
-class BOM_07_Scabbs_EdwinVanCleef_008hb:# <7>[1578]
-	""" Edwin VanCleef
-	<i>Callous bosses radicalized the Defias laborers.Now all will know their fury and pain.</i> """
-	#
-	pass
-
-if StormWind_Dangerous_Defiant:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_EdwinVanCleef_e']
-class BOM_07_Scabbs_EdwinVanCleef_e:# <7>[1578]
-	""" Dangerous Defiant
-	Increased stats. """
-	#
-	pass
-
-if StormWind_Scabbs_Cutterbutter:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_001hp']
-class BOM_07_Scabbs_Scabbs_001hp:# <7>[1578]
-	""" Scabbs Cutterbutter
-	 """
-	#
-	pass
-
-if StormWind_Dagger_Mastery:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_001p']
-class BOM_07_Scabbs_Scabbs_001p:# <7>[1578]
-	""" Dagger Mastery
-	[Hero Power]Equip a 1/2 Dagger. """
-	#
-	pass
-
-if StormWind_Scabbs_Cutterbutter:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_002hp']
-class BOM_07_Scabbs_Scabbs_002hp:# <7>[1578]
-	""" Scabbs Cutterbutter
-	 """
-	#
-	pass
-
-if StormWind_Dagger_Mastery:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_002p']
-class BOM_07_Scabbs_Scabbs_002p:# <7>[1578]
-	""" Dagger Mastery
-	[Hero Power]Equip a 1/2 Dagger. """
-	#
-	pass
-
-if StormWind_Scabbs_Cutterbutter:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_003hp']
-class BOM_07_Scabbs_Scabbs_003hp:# <7>[1578]
-	""" Scabbs Cutterbutter
-	 """
-	#
-	pass
-
-if StormWind_Poisoned_Daggers:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_003p']
-class BOM_07_Scabbs_Scabbs_003p:# <7>[1578]
-	""" Poisoned Daggers
-	[Hero Power]Equip a 2/2 Dagger. """
-	#
-	pass
-
-if StormWind_Scabbs_Cutterbutter:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_004hp']
-class BOM_07_Scabbs_Scabbs_004hp:# <7>[1578]
-	""" Scabbs Cutterbutter
-	 """
-	#
-	pass
-
-if StormWind_Poisoned_Daggers:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_004p']
-class BOM_07_Scabbs_Scabbs_004p:# <7>[1578]
-	""" Poisoned Daggers
-	[Hero Power]Equip a 2/2 Dagger. """
-	#
-	pass
-
-if StormWind_Scabbs_Cutterbutter:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_005hp']
-class BOM_07_Scabbs_Scabbs_005hp:# <7>[1578]
-	""" Scabbs Cutterbutter
-	 """
-	#
-	pass
-
-if StormWind_Scabbs_Cutterbutter:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_006hp']
-class BOM_07_Scabbs_Scabbs_006hp:# <7>[1578]
-	""" Scabbs Cutterbutter
-	 """
-	#
-	pass
-
-if StormWind_Scabbs_Cutterbutter:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_007hp']
-class BOM_07_Scabbs_Scabbs_007hp:# <7>[1578]
-	""" Scabbs Cutterbutter
-	 """
-	#
-	pass
-
-if StormWind_Scabbs_Cutterbutter:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_008hp']
-class BOM_07_Scabbs_Scabbs_008hp:# <7>[1578]
-	""" Scabbs Cutterbutter
-	 """
-	#
-	pass
-
-if StormWind_Poisoned_Daggers:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Scabbs_008p']
-class BOM_07_Scabbs_Scabbs_008p:# <7>[1578]
-	""" Poisoned Daggers
-	[Hero Power]Equip a 2/2 Dagger. """
-	#
-	pass
-
-if StormWind_Sneed:# 
-	StormWind_Rogue+=['BOM_07_Scabbs_Sneed_005hb']
-class BOM_07_Scabbs_Sneed_005hb:# <7>[1578]
-	""" Sneed
-	<i>The Defias Brotherhood has_filled_its_hideout_with traps_and_armed_guards.</i> """
-	#
-	pass
-
-if StormWind_Subtle_Butter:# 
-	StormWind_Rogue+=['BOM_07_SubtleButter_005s']
-class BOM_07_SubtleButter_005s:# <7>[1578]
-	""" Subtle Butter
-	Add four Spy Gizmosto your hand. """
-	#
-	pass
 
 if StormWind_Blackwater_Cutlass:# 
 	StormWind_Rogue+=['DED_004']
+	StormWind_Rogue+=['DED_004e']
 class DED_004:# <7>[1578]
 	""" Blackwater Cutlass
 	[Tradeable]After you [Trade] this,reduce the cost of a spellin your hand by (1). """
 	#
 	pass
 
-if StormWind_Blackwater_Treasure:# 
-	StormWind_Rogue+=['DED_004e']
 class DED_004e:# <7>[1578]
 	""" Blackwater Treasure
 	Costs (1) less. """
 	#
 	pass
+
+
+
 
 if StormWind_Parrrley:# 
 	StormWind_Rogue+=['DED_005']
@@ -266,61 +49,33 @@ class DED_005:# <7>[1578]
 	#
 	pass
 
-if StormWind_Edwin,_Defias_Kingpin:# 
+
+
+
+if StormWind_Edwin_Defias_Kingpin:# 
 	StormWind_Rogue+=['DED_510']
+	StormWind_Rogue+=['DED_510e']
+	StormWind_Rogue+=['DED_510e2']
 class DED_510:# <7>[1578]
 	""" Edwin, Defias Kingpin
 	[Battlecry:] Draw a card. If youplay it this turn, gain +2/+2and repeat this effect. """
 	#
 	pass
 
-if StormWind_Defias_Supplies:# 
-	StormWind_Rogue+=['DED_510e']
 class DED_510e:# <7>[1578]
 	""" Defias Supplies
 	If played this turn, draw a card and give Edwin +2/+2. """
 	#
 	pass
 
-if StormWind_King_of_the_Brotherhood:# 
-	StormWind_Rogue+=['DED_510e2']
 class DED_510e2:# <7>[1578]
 	""" King of the Brotherhood
 	Increased stats. """
 	#
 	pass
 
-if StormWind_Wardens_Determination:# 
-	StormWind_Rogue+=['Story_10_WardensDetermination']
-class Story_10_WardensDetermination:# <7>[1578]
-	""" Warden's Determination
-	Give +2/+2 to all minions in your hand, deck, and battlefield. """
-	#
-	pass
 
-if StormWind_Determined:# 
-	StormWind_Rogue+=['Story_10_WardensDeterminatione']
-class Story_10_WardensDeterminatione:# <7>[1578]
-	""" Determined
-	+2/+2. """
-	#
-	pass
 
-if StormWind_Wardens_Glaive:# 
-	StormWind_Rogue+=['Story_10_WardensGlaive']
-class Story_10_WardensGlaive:# <7>[1578]
-	""" Warden's Glaive
-	If this kills an enemy minion, adjacent minions go [Dormant] for 2 turns. """
-	#
-	pass
-
-if StormWind_Parrrley:# 
-	StormWind_Rogue+=['Story_11_ParrrleyPuzzle']
-class Story_11_ParrrleyPuzzle:# <7>[1578]
-	""" Parrrley
-	Swap this for a card in your opponent's deck. """
-	#
-	pass
 
 if StormWind_Maestra_of_the_Masquerade:# 
 	StormWind_Rogue+=['SW_050']
@@ -330,125 +85,125 @@ class SW_050:# <7>[1578]
 	#
 	pass
 
+
+
+
 if StormWind_Find_the_Imposter:# 
 	StormWind_Rogue+=['SW_052']
+	StormWind_Rogue+=['SW_052t']
+	StormWind_Rogue+=['SW_052t2']
+	StormWind_Rogue+=['SW_052t3']
+	StormWind_Rogue+=['SW_052t4']
+	StormWind_Rogue+=['SW_052t4e']
+	StormWind_Rogue+=['SW_052t5']
+	StormWind_Rogue+=['SW_052t6']
+	StormWind_Rogue+=['SW_052t6e']
+	StormWind_Rogue+=['SW_052t7']
+	StormWind_Rogue+=['SW_052t8_t']
 class SW_052:# <7>[1578]
 	""" Find the Imposter
 	[Questline:] Play 2SI:7 cards.[Reward:] Add a Spy Gizmo to your hand. """
 	#
 	pass
 
-if StormWind_Learn_the_Truth:# 
-	StormWind_Rogue+=['SW_052t']
 class SW_052t:# <7>[1578]
 	""" Learn the Truth
 	[Questline:] Play 2SI:7 cards.[Reward:] Add a Spy Gizmo to your hand. """
 	#
 	pass
 
-if StormWind_Marked_a_Traitor:# 
-	StormWind_Rogue+=['SW_052t2']
 class SW_052t2:# <7>[1578]
 	""" Marked a Traitor
 	[Questline:] Play 2SI:7 cards.[Reward:] Spymaster Scabbs. """
 	#
 	pass
 
-if StormWind_Spymaster_Scabbs:# 
-	StormWind_Rogue+=['SW_052t3']
 class SW_052t3:# <7>[1578]
 	""" Spymaster Scabbs
 	[Battlecry:] Add one ofeach Spy Gizmo toyour hand. """
 	#
 	pass
 
-if StormWind_Fizzflash_Distractor:# 
-	StormWind_Rogue+=['SW_052t4']
 class SW_052t4:# <7>[1578]
 	""" Fizzflash Distractor
 	Return an enemy minionto its owner's hand.They can't play it next turn. """
 	#
 	pass
 
-if StormWind_Distracted:# 
-	StormWind_Rogue+=['SW_052t4e']
 class SW_052t4e:# <7>[1578]
 	""" Distracted
 	Can't be played. """
 	#
 	pass
 
-if StormWind_Spy_o_matic:# 
-	StormWind_Rogue+=['SW_052t5']
 class SW_052t5:# <7>[1578]
 	""" Spy-o-matic
 	[Battlecry:] Look at 3 cards inyour opponent's deck.Pick one to put on top. """
 	#
 	pass
 
-if StormWind_Noggen_Fog_Generator:# 
-	StormWind_Rogue+=['SW_052t6']
 class SW_052t6:# <7>[1578]
 	""" Noggen-Fog Generator
 	Give a minion +2 Attack and [Stealth]. """
 	#
 	pass
 
-if StormWind_Noggen_Fog:# 
-	StormWind_Rogue+=['SW_052t6e']
 class SW_052t6e:# <7>[1578]
 	""" Noggen-Fog
 	+2 Attack and [Stealth]. """
 	#
 	pass
 
-if StormWind_Hidden_Gyroblade:# 
-	StormWind_Rogue+=['SW_052t7']
 class SW_052t7:# <7>[1578]
 	""" Hidden Gyroblade
 	[Deathrattle:] Throw this at a random enemy minion. """
 	#
 	pass
 
-if StormWind_Undercover_Mole:# 
-	StormWind_Rogue+=['SW_052t8_t']
 class SW_052t8_t:# <7>[1578]
 	""" Undercover Mole
 	[Stealth]. After this attacks,add a random card toyour hand <i>(from youropponent's class).</i> """
 	#
 	pass
 
+
+
+
 if StormWind_Counterfeit_Blade:# 
 	StormWind_Rogue+=['SW_310']
+	StormWind_Rogue+=['SW_310e']
 class SW_310:# <7>[1578]
 	""" Counterfeit Blade
 	[Battlecry:] Gain a randomfriendly [Deathrattle] that_triggered this game. """
 	#
 	pass
 
-if StormWind_Counterfeit_Blade:# 
-	StormWind_Rogue+=['SW_310e']
 class SW_310e:# <7>[1578]
 	""" Counterfeit Blade
 	Copied [Deathrattle] from {0}. """
 	#
 	pass
 
+
+
+
 if StormWind_Garrote:# 
 	StormWind_Rogue+=['SW_311']
+	StormWind_Rogue+=['SW_311t']
 class SW_311:# <7>[1578]
 	""" Garrote
 	Deal $2 damage to theenemy hero. Shuffle 2Bleeds into your deck thatdeal $2 more when drawn. """
 	#
 	pass
 
-if StormWind_Bleed:# 
-	StormWind_Rogue+=['SW_311t']
 class SW_311t:# <7>[1578]
 	""" Bleed
 	[Casts When Drawn]Deal $2 damage to the enemy hero. """
 	#
 	pass
+
+
+
 
 if StormWind_Sketchy_Information:# 
 	StormWind_Rogue+=['SW_405']
@@ -458,21 +213,26 @@ class SW_405:# <7>[1578]
 	#
 	pass
 
+
+
+
 if StormWind_SI7_Informant:# 
 	StormWind_Rogue+=['SW_411']
+	StormWind_Rogue+=['SW_411e']
 class SW_411:# <7>[1578]
 	""" SI:7 Informant
 	[Battlecry:] Gain +1/+1 for each other SI:7 card you've played this game. """
 	#
 	pass
 
-if StormWind_Well_Informed:# 
-	StormWind_Rogue+=['SW_411e']
 class SW_411e:# <7>[1578]
 	""" Well Informed
 	+1/+1 """
 	#
 	pass
+
+
+
 
 if StormWind_SI7_Extortion:# 
 	StormWind_Rogue+=['SW_412']
@@ -482,6 +242,9 @@ class SW_412:# <7>[1578]
 	#
 	pass
 
+
+
+
 if StormWind_SI7_Operative:# 
 	StormWind_Rogue+=['SW_413']
 class SW_413:# <7>[1578]
@@ -490,6 +253,9 @@ class SW_413:# <7>[1578]
 	#
 	pass
 
+
+
+
 if StormWind_SI7_Assassin:# 
 	StormWind_Rogue+=['SW_417']
 class SW_417:# <7>[1578]
@@ -497,6 +263,9 @@ class SW_417:# <7>[1578]
 	Costs (1) less for each SI:7card you've played this game.[Combo:] Destroy anenemy minion. """
 	#
 	pass
+
+
+
 
 if StormWind_Loan_Shark:# 
 	StormWind_Rogue+=['SW_434']
