abd0a5195789d00b91f7cb1d5f4420340bfff65a
Kazushi Ahara <ahara@meiji.ac.jp>
2022-08-12 23:01:04 +0900

    Alterac_DemonHunter ファイル整備

--- fireplaceAharaLab/fireplace/cards/alterac/alterac_demon_hunter.py
+++ fireplaceAharaLab/fireplace/cards/alterac/alterac_demon_hunter.py
@@ -1,115 +1,197 @@
+
 from ..utils import *
 
-#Alterac_Demonhunter=['AV_118','AV_204','AV_204e','AV_204p','AV_204t2','AV_209','AV_261','AV_261e','AV_262','AV_262e2','AV_264','AV_265','AV_267','AV_267e2','AV_269','AV_269e','AV_269t','AV_661','AV_661e2',]
-#+= ['ONY_014','ONY_014e','ONY_016','ONY_016t','ONY_016t2','ONY_036',]
+Alterac_DemonHunter=[]
+
+
+Alterac_Battleworn_Vanguard=True
+Alterac_Kurtrus,_Demon_Render=True
+Alterac_Ashfallens_Power=True
+Alterac_Ashfallens_Fury=True
+Alterac_Felbat_Shrieker=True
+Alterac_Dreadprison_Glaive=True
+Alterac_Flag_Runner=True
+Alterac_Team_Player=True
+Alterac_Warden_of_Chains=True
+Alterac_Terrifying=True
+Alterac_Sigil_of_Reckoning=True
+Alterac_Urzul_Giant=True
+Alterac_Caria_Felsoul=True
+Alterac_Demonic=True
+Alterac_Flanking_Maneuver=True
+Alterac_Woe_Is_Me=True
+Alterac_Snowy_Satyr=True
+Alterac_Field_of_Strife=True
+Alterac_Empowered=True
+Alterac_Kurtrus=True
+Alterac_Kurtrus=True
+Alterac_Kurtrus=True
+Alterac_Kurtrus=True
+Alterac_Kurtrus,_At_Peace=True
+Alterac_Keen_Reflex=True
+Alterac_Keen_Reflex=True
+Alterac_Wings_of_Hate_Rank_1=True
+Alterac_Wings_of_Hate_(Rank_2)=True
+Alterac_Wings_of_Hate_(Rank_3)=True
+Alterac_Razorglaive_Sentinel=True
+Alterac_Kurtrus=True
+
+
+if Alterac_Battleworn_Vanguard:# 
+	Alterac_DemonHunter+=['AV_118']
 class AV_118:# <14>[1626]
 	""" Battleworn Vanguard
 	After your hero attacks,summon two 1/1 Felwings. """
 	#
 	pass
 
+
+
+
+if Alterac_Kurtrus,_Demon_Render:# 
+	Alterac_DemonHunter+=['AV_204']
+	Alterac_DemonHunter+=['AV_204e']
+	Alterac_DemonHunter+=['AV_204p']
+	Alterac_DemonHunter+=['AV_204t2']
 class AV_204:# <14>[1626]
 	""" Kurtrus, Demon-Render
 	[Battlecry:] Summon two@/4 Demons with [Rush].<i>(Improved by your heroattacks this game.)</i> """
 	#
 	pass
-
 class AV_204e:# <14>[1626]
 	""" Ashfallen's Power
 	+2 Attack this turn. """
 	#
 	pass
-
 class AV_204p:# <14>[1626]
 	""" Ashfallen's Fury
 	[Hero Power]+2 Attack this turn.After a friendly minion attacks, refresh this. """
 	#
 	pass
-
 class AV_204t2:# <14>[1626]
 	""" Felbat Shrieker
 	[Rush] """
 	#
 	pass
 
+
+
+
+if Alterac_Dreadprison_Glaive:# 
+	Alterac_DemonHunter+=['AV_209']
 class AV_209:# <14>[1626]
 	""" Dreadprison Glaive
 	[Honorable Kill:] Dealdamage equal to yourhero's Attack to theenemy hero. """
 	#
 	pass
 
+
+
+
+if Alterac_Flag_Runner:# 
+	Alterac_DemonHunter+=['AV_261']
+	Alterac_DemonHunter+=['AV_261e']
 class AV_261:# <14>[1626]
 	""" Flag Runner
 	Whenever a friendly minion dies, gain +1 Attack. """
 	#
 	pass
-
 class AV_261e:# <14>[1626]
 	""" Team Player
 	+1 Attack """
 	#
 	pass
 
+
+
+
+if Alterac_Warden_of_Chains:# 
+	Alterac_DemonHunter+=['AV_262']
+	Alterac_DemonHunter+=['AV_262e2']
 class AV_262:# <14>[1626]
 	""" Warden of Chains
 	[Taunt][Battlecry:] If you're holdinga Demon that costs (5) ormore, gain +1/+2. """
 	#
 	pass
-
 class AV_262e2:# <14>[1626]
 	""" Terrifying
 	+1/+2. """
 	#
 	pass
 
+
+
+
+if Alterac_Sigil_of_Reckoning:# 
+	Alterac_DemonHunter+=['AV_264']
 class AV_264:# <14>[1626]
 	""" Sigil of Reckoning
 	At the start of your next turn, summon a random Demon from your hand. """
 	#
 	pass
 
+
+
+
+if Alterac_Urzul_Giant:# 
+	Alterac_DemonHunter+=['AV_265']
 class AV_265:# <14>[1626]
 	""" Ur'zul Giant
 	Costs (1) less for each friendly minion that died this game. """
 	#
 	pass
 
+
+
+
+if Alterac_Caria_Felsoul:# 
+	Alterac_DemonHunter+=['AV_267']
+	Alterac_DemonHunter+=['AV_267e2']
 class AV_267:# <14>[1626]
 	""" Caria Felsoul
-	[Battlecry:] Transform into a 6/6 copy of a Demon in your deck. """
+	[Battlecry:] Transform into a 7/7 copy of a Demon in your deck. """
 	#
 	pass
-
 class AV_267e2:# <14>[1626]
 	""" Demonic
-	Attack and Health set to 6. """
+	Attack and Health set to 7. """
 	#
 	pass
 
+
+
+
+if Alterac_Flanking_Maneuver:# 
+	Alterac_DemonHunter+=['AV_269']
+	Alterac_DemonHunter+=['AV_269e']
+	Alterac_DemonHunter+=['AV_269t']
 class AV_269:# <14>[1626]
 	""" Flanking Maneuver
 	Summon a 4/2 Demon with [Rush]. If it dies this turn, summon another. """
 	#
 	pass
-
 class AV_269e:# <14>[1626]
 	""" Woe Is Me
 	If this dies summon another. """
 	#
 	pass
-
 class AV_269t:# <14>[1626]
 	""" Snowy Satyr
 	[Rush] """
 	#
 	pass
 
+
+
+
+if Alterac_Field_of_Strife:# 
+	Alterac_DemonHunter+=['AV_661']
+	Alterac_DemonHunter+=['AV_661e2']
 class AV_661:# <14>[1626]
 	""" Field of Strife
 	Your minions have+1 Attack.Lasts 3 turns. """
 	#
 	pass
-
 class AV_661e2:# <14>[1626]
 	""" Empowered
 	+1 Attack from {0}. """
@@ -117,38 +199,54 @@ class AV_661e2:# <14>[1626]
 	pass
 
 
+
+
+if Alterac_Keen_Reflex:# 
+	Alterac_DemonHunter+=['ONY_014']
+	Alterac_DemonHunter+=['ONY_014e']
 class ONY_014:# <14>[1626]
 	""" Keen Reflex
 	Deal $1 damage to allminions. [Honorable Kill:]Gain +1 Attack this turn. """
 	#
 	pass
-
 class ONY_014e:# <14>[1626]
 	""" Keen Reflex
 	+1 Attack this turn. """
 	#
 	pass
 
+
+
+
+if Alterac_Wings_of_Hate_Rank_1:# 
+	Alterac_DemonHunter+=['ONY_016']
+	Alterac_DemonHunter+=['ONY_016t']
+	Alterac_DemonHunter+=['ONY_016t2']
 class ONY_016:# <14>[1626]
 	""" Wings of Hate (Rank 1)
 	Summon two 1/1Felwings. <i>(Upgradeswhen you have 5 Mana.)</i> """
 	#
 	pass
-
 class ONY_016t:# <14>[1626]
 	""" Wings of Hate (Rank 2)
 	Summon three 1/1Felwings. <i>(Upgradeswhen you have 10 Mana.)</i> """
 	#
 	pass
-
 class ONY_016t2:# <14>[1626]
 	""" Wings of Hate (Rank 3)
 	Summon four1/1 Felwings. """
 	#
 	pass
 
+
+
+
+if Alterac_Razorglaive_Sentinel:# 
+	Alterac_DemonHunter+=['ONY_036']
 class ONY_036:# <14>[1626]
 	""" Razorglaive Sentinel
 	After you play the left or right-most card in your hand, draw a card. """
 	#
 	pass
+
+
