ab88a4c64796894a162146232fb7b296ad1f77f1
Kazushi Ahara <ahara@meiji.ac.jp>
2022-08-12 22:54:34 +0900

    Alterac_Warriorだん

--- fireplaceAharaLab/fireplace/cards/alterac/alterac_warrior.py
+++ fireplaceAharaLab/fireplace/cards/alterac/alterac_warrior.py
@@ -1,7 +1,25 @@
 from ..utils import *
 
-#Alterac_Warrior=['AV_108','AV_109','AV_109e','AV_119','AV_119e','AV_145','AV_145e','AV_202','AV_202p','AV_202t2','AV_321','AV_322','AV_323','AV_323t','AV_565','AV_660',]
+Alterac_Warrior=[]
 
+Alterac_Shield_Shatter=True  ###
+Alterac_Frozen_Buckler=True  ###
+Alterac_To_the_Front!=True  ###
+Alterac_Captain_Galvangar=True  ###
+Alterac_Rokara_the_Valorous=True  ###
+Alterac_Glory_Chaser=True  ###
+Alterac_Snowed_In=True  ###
+Alterac_Scrapsmith=True  ###
+Alterac_Axe_Berserker=True  ###
+Alterac_Iceblood_Garrison=True  ###
+Alterac_Hit_It_Very_Hard=True  ###
+Alterac_Onyxian_Drake=True  ###
+Alterac_Shoulder_Check=True  ###
+
+
+
+if Alterac_Shield_Shatter:# 
+	Alterac_Warrior+=['AV_108']
 class AV_108:
 	"""Shield Shatter (10) frost
 	Deal 5 damage to all minions. Costs (1) less for each Armor you have. """
@@ -9,6 +27,12 @@ class AV_108:
 	play = Hit(ALL_MINIONS, 5)
 	pass
 
+
+
+
+if Alterac_Frozen_Buckler:# 
+	Alterac_Warrior+=['AV_109']
+	Alterac_Warrior+=['AV_109e']
 class AV_109:#
 	""" Frozen Buckler (2) frost
 	Gain 10 Armor. At the start of your next turn, lose 5 Armor. """
@@ -17,6 +41,12 @@ class AV_109:#
 class AV_109e:
 	events = OWN_TURN_BEGIN.on(GainArmor(FRIENDLY_HERO, -5), Destroy(SELF))
 
+
+
+
+if Alterac_To_the_Front!:# 
+	Alterac_Warrior+=['AV_119']
+	Alterac_Warrior+=['AV_119e']
 class AV_119:
 	""" To the Front! (2) 
 	Your minions cost (2) less this turn (but not less than 1). """
@@ -27,6 +57,12 @@ class AV_119e:
 	#cost = lambda self, i: max(1,i-2)
 	events = OWN_TURN_END.on(Destroy(SELF))
 
+
+
+
+if Alterac_Captain_Galvangar:# 
+	Alterac_Warrior+=['AV_145']
+	Alterac_Warrior+=['AV_145e']
 class AV_145_Action(TargetedAction):
 	TARGET = ActionArg()
 	def do (self,source, target):
@@ -40,6 +76,13 @@ class AV_145:##events?
 	pass
 AV_145e=buff(atk=3, health=3, charge=True)
 
+
+
+
+if Alterac_Rokara_the_Valorous:# 
+	Alterac_Warrior+=['AV_202']
+	Alterac_Warrior+=['AV_202p']
+	Alterac_Warrior+=['AV_202t2']
 class AV_202:
 	""" Rokara, the Valorous (7/*/5) hero
 	Battlecry: Equip a 5/2 Unstoppable Force. """
@@ -55,12 +98,22 @@ class AV_202t2:
 	""" The Unstoppable Force  """
 	pass
 
+
+
+
+if Alterac_Glory_Chaser:# 
+	Alterac_Warrior+=['AV_321']
 class AV_321:
 	""" Glory Chaser (3/4/3)
 	After you play a Taunt minion, draw a card. """
 	events = Play(CONTROLLER, FRIENDLY + TAUNT).on(Draw(CONTROLLER))
 	pass
 
+
+
+
+if Alterac_Snowed_In:# 
+	Alterac_Warrior+=['AV_322']
 class AV_322:
 	""" Snowed In (3) frost
 	Destroy a damaged minion. Freeze all other minions. """
@@ -71,6 +124,12 @@ class AV_322:
 		)
 	pass
 
+
+
+
+if Alterac_Scrapsmith:# 
+	Alterac_Warrior+=['AV_323']
+	Alterac_Warrior+=['AV_323t']
 class AV_323:
 	""" Scrapsmith (3/2/4)
 	Taunt Battlecry: Add two 2/4 Grunts with Taunt to your hand. """
@@ -80,12 +139,22 @@ class AV_323t:
 	"""   """
 	pass
 
+
+
+
+if Alterac_Axe_Berserker:# 
+	Alterac_Warrior+=['AV_565']
 class AV_565:
 	""" Axe Berserker (4/3/5)
 	Rush. Honorable Kill: Draw a weapon. """
 	honorable_kill = Give(CONTROLLER, RANDOM(FRIENDLY_DECK + WEAPON))
 	pass
 
+
+
+
+if Alterac_Iceblood_Garrison:# 
+	Alterac_Warrior+=['AV_660']
 class AV_660:
 	""" Iceblood Garrison (2)
 	At the end of your turn, deal 1 damage to all minions. Lasts 3 turns. """
@@ -97,20 +166,26 @@ class AV_660:
 	pass
 
 
+
+
+if Alterac_Hit_It_Very_Hard:# 
+	Alterac_Warrior+=['ONY_023']
+	Alterac_Warrior+=['ONY_023e']
 class ONY_023:# <10>[1626]
 	""" Hit It Very Hard
 	Gain +10 Attack and "Can't attack heroes" this turn. """
 	play = Buff(FRIENDLY_HERO, 'ONY_023e')
 	pass
-
 ONY_023e=buff(10,0,cannot_attack_heroes = True)
-#class ONY_023e:# <10>[1626]
-""" HIT IT HARD
-+10 Attack this turn. """
+""" HIT IT HARD +10 Attack this turn. """
 #TAG_ONE_TURN_EFFECT
 ## see SCH_138e2
-#pass
 
+
+
+
+if Alterac_Onyxian_Drake:# 
+	Alterac_Warrior+=['ONY_024']
 class ONY_024:# <10>[1626]
 	""" Onyxian Drake
 	[Taunt] [Battlecry:] Deal damage equal to your Armor to an enemy minion. """
@@ -118,6 +193,11 @@ class ONY_024:# <10>[1626]
 	play = Hit(TARGET, ARMOR(FRIENDLY_HERO))
 	pass
 
+
+
+
+if Alterac_Shoulder_Check:# 
+	Alterac_Warrior+=['ONY_025']
 class ONY_025:# <10>[1626]
 	""" Shoulder Check
 	[Tradeable]Give a minion +2/+1 and [Rush]. """
@@ -125,3 +205,5 @@ class ONY_025:# <10>[1626]
 	play = Buff(TARGET, 'ONY_025e')
 	pass
 ONY_025e=buff(2,1,rush=True)#<12>[1626]
+
+####################################################
