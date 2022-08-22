62af154e377dd03e95d4da81383f08ea51d724e2
Kazushi Ahara <ahara@meiji.ac.jp>
2022-08-23 00:37:35 +0900

    Classic_Warlock

--- fireplaceAharaLab/fireplace/cards/classic/classic_warlock.py
+++ fireplaceAharaLab/fireplace/cards/classic/classic_warlock.py
@@ -2,10 +2,6 @@ from ..utils import *
 
 Classic_Warlock=[]
 
-Classic_Demonfire=True
-Classic_Doomguard=True
-Classic_Sense_Demons=True
-Classic_Shadow_Bolt=True
 Classic_Shadow_Bolt=True
 Classic_Blood_Imp=True
 Classic_Drain_Life=True
@@ -32,64 +28,36 @@ Classic_Lord_Jaraxxus=True
 Classic_Demonfire=True
 Classic_INFERNO=True
 Classic_Infernal=True
-Classic_Life_Tap=True
+Classic_Life_Tap=False ## HP
 Classic_Sacrificial_Pact=True
 
-
-if Classic_Demonfire:# 
-	Classic_Warlock+=['Story_09_DemonfirePuzzle']
-class Story_09_DemonfirePuzzle:# <9>[1646]
-	""" Demonfire
-	Deal $2 damage to a minion. If itÅfs a friendly Demon, give it +2/+2 instead. """
-	#
-	pass
-
-if Classic_Doomguard:# 
-	Classic_Warlock+=['Story_09_DoomguardPuzzle']
-class Story_09_DoomguardPuzzle:# <9>[1646]
-	""" Doomguard
-	[Charge]. [Battlecry:] Discard two random cards. """
-	#
-	pass
-
-if Classic_Sense_Demons:# 
-	Classic_Warlock+=['Story_09_SenseDemons']
-class Story_09_SenseDemons:# <9>[1646]
-	""" Sense Demons
-	Draw 2 Demonsfrom your deck. """
-	#
-	pass
-
-if Classic_Shadow_Bolt:# 
-	Classic_Warlock+=['Story_09_Shadowbolt']
-class Story_09_Shadowbolt:# <9>[1646]
-	""" Shadow Bolt
-	Deal $4 damageto a minion. """
-	#
-	pass
 
 if Classic_Shadow_Bolt:# 
 	Classic_Warlock+=['VAN_CS2_057']
 class VAN_CS2_057:# <9>[1646]
 	""" Shadow Bolt
 	Deal $4 damageto a minion. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(TARGET, 4)
 	pass
 
 if Classic_Blood_Imp:# 
-	Classic_Warlock+=['VAN_CS2_059']
+	Classic_Warlock+=['VAN_CS2_059','CS2_059o']
 class VAN_CS2_059:# <9>[1646]
 	""" Blood Imp
 	  [Stealth]. At the end of your  turn, give another random friendly minion +1 Health. """
-	#
+	events = OWN_TURN_END.on(Buff(RANDOM_OTHER_FRIENDLY_MINION, "CS2_059o"))
 	pass
+CS2_059o = buff(health=1)
+
 
 if Classic_Drain_Life:# 
 	Classic_Warlock+=['VAN_CS2_061']
 class VAN_CS2_061:# <9>[1646]
 	""" Drain Life
 	Deal $2 damage. Restore #2 Health to your hero. """
-	#
+	requirements = {PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(TARGET, 2), Heal(FRIENDLY_HERO, 2)
 	pass
 
 if Classic_Hellfire:# 
@@ -97,23 +65,29 @@ if Classic_Hellfire:#
 class VAN_CS2_062:# <9>[1646]
 	""" Hellfire
 	Deal $3 damage to ALL_characters. """
-	#
+	play = Hit(ALL_CHARACTERS, 3)
 	pass
 
 if Classic_Corruption:# 
-	Classic_Warlock+=['VAN_CS2_063']
+	Classic_Warlock+=['VAN_CS2_063','CS2_063e']
 class VAN_CS2_063:# <9>[1646]
 	""" Corruption
 	Choose an enemy minion. At the start of your turn, destroy it. """
-	#
+	requirements = {
+		PlayReq.REQ_ENEMY_TARGET: 0,
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Buff(TARGET, "CS2_063e")
 	pass
+class CS2_063e:
+	events = OWN_TURN_BEGIN.on(Destroy(OWNER))
 
 if Classic_Dread_Infernal:# 
 	Classic_Warlock+=['VAN_CS2_064']
 class VAN_CS2_064:# <9>[1646]
 	""" Dread Infernal
 	[Battlecry:] Deal 1 damage to ALL other characters. """
-	#
+	play = Hit(ALL_CHARACTERS - SELF, 1)
 	pass
 
 if Classic_Voidwalker:# 
@@ -129,7 +103,7 @@ if Classic_Felguard:#
 class VAN_EX1_301:# <9>[1646]
 	""" Felguard
 	[Taunt][Battlecry:] Destroy one of your Mana Crystals. """
-	#
+	play = GainEmptyMana(CONTROLLER, -1)
 	pass
 
 if Classic_Mortal_Coil:# 
@@ -137,7 +111,8 @@ if Classic_Mortal_Coil:#
 class VAN_EX1_302:# <9>[1646]
 	""" Mortal Coil
 	Deal $1 damage to a minion. If that kills it, draw a card. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(TARGET, 1), Dead(TARGET) & Draw(CONTROLLER)
 	pass
 
 if Classic_Shadowflame:# 
@@ -145,7 +120,11 @@ if Classic_Shadowflame:#
 class VAN_EX1_303:# <9>[1646]
 	""" Shadowflame
 	Destroy a friendly minion and deal its Attack damage to all enemy minions. """
-	#
+	requirements = {
+		PlayReq.REQ_FRIENDLY_TARGET: 0,
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(ENEMY_MINIONS, ATK(TARGET)), Destroy(TARGET)
 	pass
 
 if Classic_Void_Terror:# 
@@ -153,7 +132,10 @@ if Classic_Void_Terror:#
 class VAN_EX1_304:# <9>[1646]
 	""" Void Terror
 	[Battlecry:] Destroy bothadjacent minions and gain their Attack and Health. """
-	#
+	play = (
+		Buff(SELF, "EX1_304e", atk=ATK(SELF_ADJACENT), max_health=CURRENT_HEALTH(SELF_ADJACENT)),
+		Destroy(SELF_ADJACENT)
+	)
 	pass
 
 if Classic_Felstalker:# 
@@ -161,7 +143,7 @@ if Classic_Felstalker:#
 class VAN_EX1_306:# <9>[1646]
 	""" Felstalker
 	[Battlecry:] Discard a random card. """
-	#
+	play = Discard(RANDOM(FRIENDLY_HAND))
 	pass
 
 if Classic_Soulfire:# 
@@ -169,7 +151,8 @@ if Classic_Soulfire:#
 class VAN_EX1_308:# <9>[1646]
 	""" Soulfire
 	Deal $4 damage.Discard a random card. """
-	#
+	requirements = {PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(TARGET, 4), Discard(RANDOM(FRIENDLY_HAND))
 	pass
 
 if Classic_Siphon_Soul:# 
@@ -177,7 +160,8 @@ if Classic_Siphon_Soul:#
 class VAN_EX1_309:# <9>[1646]
 	""" Siphon Soul
 	Destroy a minion. Restore #3 Health to_your hero. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Destroy(TARGET), Heal(FRIENDLY_HERO, 3)
 	pass
 
 if Classic_Doomguard:# 
@@ -185,7 +169,7 @@ if Classic_Doomguard:#
 class VAN_EX1_310:# <9>[1646]
 	""" Doomguard
 	[Charge]. [Battlecry:] Discard two random cards. """
-	#
+	play = Discard(RANDOM(FRIENDLY_HAND) * 2)
 	pass
 
 if Classic_Twisting_Nether:# 
@@ -193,7 +177,7 @@ if Classic_Twisting_Nether:#
 class VAN_EX1_312:# <9>[1646]
 	""" Twisting Nether
 	Destroy all minions. """
-	#
+	play = Destroy(ALL_MINIONS)
 	pass
 
 if Classic_Pit_Lord:# 
@@ -201,7 +185,7 @@ if Classic_Pit_Lord:#
 class VAN_EX1_313:# <9>[1646]
 	""" Pit Lord
 	[Battlecry:] Deal 5 damage to your hero. """
-	#
+	play = Hit(FRIENDLY_HERO, 5)
 	pass
 
 if Classic_Summoning_Portal:# 
@@ -209,23 +193,39 @@ if Classic_Summoning_Portal:#
 class VAN_EX1_315:# <9>[1646]
 	""" Summoning Portal
 	Your minions cost (2) less, but not less than (1). """
-	#
+	update = Refresh(FRIENDLY_HAND + MINION, {
+		GameTag.COST: lambda self, i: min(i, max(1, i - 2))
+	})
 	pass
 
 if Classic_Power_Overwhelming:# 
-	Classic_Warlock+=['VAN_EX1_316']
+	Classic_Warlock+=['VAN_EX1_316','EX1_316e']
 class VAN_EX1_316:# <9>[1646]
 	""" Power Overwhelming
 	Give a friendly minion +4/+4 until end of turn. Then, it dies. Horribly. """
-	#
+	requirements = {
+		PlayReq.REQ_FRIENDLY_TARGET: 0,
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Buff(TARGET, "EX1_316e")
 	pass
+class EX1_316e:
+	events = TURN_END.on(Destroy(OWNER))
+	tags = {
+		GameTag.ATK: +4,
+		GameTag.HEALTH: +4,
+	}
 
 if Classic_Sense_Demons:# 
 	Classic_Warlock+=['VAN_EX1_317']
 class VAN_EX1_317:# <9>[1646]
 	""" Sense Demons
 	Draw 2 Demonsfrom your deck. """
-	#
+	play = (
+		Find(FRIENDLY_DECK + DEMON) &
+		ForceDraw(RANDOM(FRIENDLY_DECK + DEMON)) |
+		Give(CONTROLLER, "EX1_317t"),
+	) * 2
 	pass
 
 if Classic_Flame_Imp:# 
@@ -233,7 +233,7 @@ if Classic_Flame_Imp:#
 class VAN_EX1_319:# <9>[1646]
 	""" Flame Imp
 	[Battlecry:] Deal 3 damage to your hero. """
-	#
+	play = Hit(FRIENDLY_HERO, 3)
 	pass
 
 if Classic_Bane_of_Doom:# 
@@ -241,7 +241,8 @@ if Classic_Bane_of_Doom:#
 class VAN_EX1_320:# <9>[1646]
 	""" Bane of Doom
 	Deal $2 damage to_a character. If that kills it, summon a random Demon. """
-	#
+	requirements = {PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(TARGET, 2), Dead(TARGET) & Summon(CONTROLLER, RandomMinion(race=Race.DEMON))
 	pass
 
 if Classic_Lord_Jaraxxus:# 
@@ -249,23 +250,30 @@ if Classic_Lord_Jaraxxus:#
 class VAN_EX1_323:# <9>[1646]
 	""" Lord Jaraxxus
 	[Battlecry:] Destroy your hero and replace it with Lord Jaraxxus. """
-	#
+	play = (
+		Summon(CONTROLLER, "EX1_323h").then(Morph(SELF, Summon.CARD)),
+		Summon(CONTROLLER, "EX1_323w")
+	)
 	pass
 
 if Classic_Demonfire:# 
-	Classic_Warlock+=['VAN_EX1_596']
+	Classic_Warlock+=['VAN_EX1_596','EX1_596e']
 class VAN_EX1_596:# <9>[1646]
 	""" Demonfire
 	Deal $2 damage to a minion. If itÅfs a friendly Demon, give it +2/+2 instead. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Find(TARGET + FRIENDLY + DEMON) & Buff(TARGET, "EX1_596e") | Hit(TARGET, 2)
 	pass
+EX1_596e = buff(+2, +2)
+
 
 if Classic_INFERNO:# 
 	Classic_Warlock+=['VAN_EX1_tk33']
 class VAN_EX1_tk33:# <9>[1646]
 	""" INFERNO!
 	[Hero Power]Summon a 6/6 Infernal. """
-	#
+	requirements = {PlayReq.REQ_NUM_MINION_SLOTS: 1}
+	activate = Summon(CONTROLLER, "EX1_tk34")
 	pass
 
 if Classic_Infernal:# 
@@ -278,13 +286,12 @@ class VAN_EX1_tk34:# <9>[1646]
 
 if Classic_Life_Tap:# 
 	Classic_Warlock+=['VAN_HERO_07bp']
+	Classic_Warlock+=['VAN_HERO_07bp2']
 class VAN_HERO_07bp:# <9>[1646]
 	""" Life Tap
 	[Hero Power]Draw a card and take $2_damage. """
 	#
 	pass
-
-	Classic_Warlock+=['VAN_HERO_07bp2']
 class VAN_HERO_07bp2:# <9>[1646]
 	""" Soul Tap
 	[Hero Power]Draw a card. """
@@ -296,6 +303,10 @@ if Classic_Sacrificial_Pact:#
 class VAN_NEW1_003:# <9>[1646]
 	""" Sacrificial Pact
 	Destroy a Demon. Restore #5 Health to your hero. """
-	#
+	requirements = {
+		PlayReq.REQ_FRIENDLY_TARGET: 0,
+		PlayReq.REQ_TARGET_IF_AVAILABLE: 0,
+		PlayReq.REQ_TARGET_WITH_RACE: 15}
+	play = Destroy(TARGET), Heal(FRIENDLY_HERO, 5)
 	pass
 
--- fireplaceAharaLab/fireplace/cards/classic/classic_warrior.py
+++ fireplaceAharaLab/fireplace/cards/classic/classic_warrior.py
@@ -2,7 +2,6 @@ from ..utils import *
 
 Classic_Warrior=[]
 
-Classic_Armor_Up=True
 Classic_Charge=True
 Classic_Rampage=True
 Classic_Heroic_Strike=True
@@ -26,63 +25,47 @@ Classic_Cruel_Taskmaster=True
 Classic_Frothing_Berserker=True
 Classic_Shield_Block=True
 Classic_Inner_Rage=True
-Classic_Armor_Up=True
+Classic_Armor_Up=False # HP
 Classic_Korkron_Elite=True
 Classic_Commanding_Shout=True
 
 
-if Classic_Armor_Up:# 
-	Classic_Warrior+=['VAN_CS2_102_H3']
-class VAN_CS2_102_H3:# <10>[1646]
-	""" Armor Up!
-	[Hero Power]Gain 2 Armor. """
-	#
-	pass
 
 if Classic_Charge:# 
 	Classic_Warrior+=['VAN_CS2_103']
+	Classic_Warrior+=['VAN_CS2_103e2']
 class VAN_CS2_103:# <10>[1646]
 	""" Charge
 	Give a friendly minion +2 Attack and [Charge]. """
-	#
+	requirements = {
+		PlayReq.REQ_FRIENDLY_TARGET: 0,
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Buff(TARGET, "CS2_103e2")
 	pass
-
-	Classic_Warrior+=['VAN_CS2_103_Puzzle']
-class VAN_CS2_103_Puzzle:# <10>[1646]
-	""" Charge
-	Give a friendly minion [Charge]. It can't attack heroes this turn. """
-	#
-	pass
-
-	Classic_Warrior+=['VAN_CS2_103e2']
-class VAN_CS2_103e2:# <10>[1646]
-	""" Charge
-	+2 Attack and [Charge]. """
-	#
-	pass
-
-	Classic_Warrior+=['VAN_CS2_103e2_Puzzle']
-class VAN_CS2_103e2_Puzzle:# <10>[1646]
-	""" Charge
-	Has [Charge]. """
-	#
-	pass
+VAN_CS2_103e2 = buff(atk=2, charge=True)
 
 if Classic_Rampage:# 
-	Classic_Warrior+=['VAN_CS2_104']
+	Classic_Warrior+=['VAN_CS2_104','CS2_104e']
 class VAN_CS2_104:# <10>[1646]
 	""" Rampage
 	Give a damaged minion +3/+3. """
-	#
+	requirements = {
+		PlayReq.REQ_DAMAGED_TARGET: 0,
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Buff(TARGET, "CS2_104e")
 	pass
+CS2_104e = buff(+3, +3)
 
 if Classic_Heroic_Strike:# 
-	Classic_Warrior+=['VAN_CS2_105']
+	Classic_Warrior+=['VAN_CS2_105','CS2_105e']
 class VAN_CS2_105:# <10>[1646]
 	""" Heroic Strike
 	Give your hero +4_Attack this turn. """
-	#
+	play = Buff(FRIENDLY_HERO, "CS2_105e")
 	pass
+CS2_105e = buff(atk=4)
 
 if Classic_Fiery_War_Axe:# 
 	Classic_Warrior+=['VAN_CS2_106']
@@ -97,7 +80,12 @@ if Classic_Execute:#
 class VAN_CS2_108:# <10>[1646]
 	""" Execute
 	Destroy a damaged enemy minion. """
-	#
+	requirements = {
+		PlayReq.REQ_DAMAGED_TARGET: 0,
+		PlayReq.REQ_ENEMY_TARGET: 0,
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Destroy(TARGET)
 	pass
 
 if Classic_Arcanite_Reaper:# 
@@ -113,23 +101,26 @@ if Classic_Cleave:#
 class VAN_CS2_114:# <10>[1646]
 	""" Cleave
 	Deal $2 damage totwo random enemyminions. """
-	#
+	requirements = {PlayReq.REQ_MINIMUM_ENEMY_MINIONS: 1}
+	play = Hit(RANDOM_ENEMY_MINION * 2, 2)
 	pass
 
 if Classic_Warsong_Commander:# 
-	Classic_Warrior+=['VAN_EX1_084']
+	Classic_Warrior+=['VAN_EX1_084','EX1_084e']
 class VAN_EX1_084:# <10>[1646]
 	""" Warsong Commander
 	Whenever you summon a minion with 3 or less Attack, give it [Charge]. """
-	#
+	update = Refresh(FRIENDLY_MINIONS + CHARGE, buff="EX1_084e")
 	pass
+EX1_084e = buff(atk=1)
 
 if Classic_Slam:# 
 	Classic_Warrior+=['VAN_EX1_391']
 class VAN_EX1_391:# <10>[1646]
 	""" Slam
 	Deal $2 damage to a minion. If it survives, draw a card. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(TARGET, 2), Dead(TARGET) | Draw(CONTROLLER)
 	pass
 
 if Classic_Battle_Rage:# 
@@ -137,15 +128,18 @@ if Classic_Battle_Rage:#
 class VAN_EX1_392:# <10>[1646]
 	""" Battle Rage
 	Draw a card for each damaged friendly character. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0}
+	play = Draw(CONTROLLER) * Count(FRIENDLY_CHARACTERS + DAMAGED)
 	pass
 
 if Classic_Arathi_Weaponsmith:# 
-	Classic_Warrior+=['VAN_EX1_398']
+	Classic_Warrior+=['VAN_EX1_398','EX1_398t']
 class VAN_EX1_398:# <10>[1646]
 	""" Arathi Weaponsmith
 	[Battlecry:] Equip a 2/2_weapon. """
-	#
+	play = Summon(CONTROLLER, "EX1_398t")
+	pass
+class EX1_398t:
 	pass
 
 if Classic_Whirlwind:# 
@@ -153,7 +147,7 @@ if Classic_Whirlwind:#
 class VAN_EX1_400:# <10>[1646]
 	""" Whirlwind
 	Deal $1 damage to ALL_minions. """
-	#
+	play = Hit(ALL_MINIONS, 1)
 	pass
 
 if Classic_Armorsmith:# 
@@ -161,7 +155,7 @@ if Classic_Armorsmith:#
 class VAN_EX1_402:# <10>[1646]
 	""" Armorsmith
 	Whenever a friendly minion_takes damage, gain 1 Armor. """
-	#
+	events = Damage(FRIENDLY_MINIONS).on(GainArmor(FRIENDLY_HERO, 1))
 	pass
 
 if Classic_Brawl:# 
@@ -169,7 +163,12 @@ if Classic_Brawl:#
 class VAN_EX1_407:# <10>[1646]
 	""" Brawl
 	Destroy all minions except one. <i>(chosen randomly)</i> """
-	#
+	requirements = {PlayReq.REQ_MINIMUM_TOTAL_MINIONS: 2}
+	play = (
+		Find(ALL_MINIONS + ALWAYS_WINS_BRAWLS) &
+		Destroy(ALL_MINIONS - RANDOM(ALL_MINIONS + ALWAYS_WINS_BRAWLS)) |
+		Destroy(ALL_MINIONS - RANDOM_MINION)
+	)
 	pass
 
 if Classic_Mortal_Strike:# 
@@ -177,23 +176,31 @@ if Classic_Mortal_Strike:#
 class VAN_EX1_408:# <10>[1646]
 	""" Mortal Strike
 	Deal $4 damage. If you have 12 or less Health, deal $6 instead. """
-	#
+	requirements = {PlayReq.REQ_TARGET_TO_PLAY: 0}
+	powered_up = CURRENT_HEALTH(FRIENDLY_HERO) <= 12
+	play = powered_up & Hit(TARGET, 6) | Hit(TARGET, 4)
 	pass
 
 if Classic_Upgrade:# 
-	Classic_Warrior+=['VAN_EX1_409']
+	Classic_Warrior+=['VAN_EX1_409','EX1_409e']
 class VAN_EX1_409:# <10>[1646]
 	""" Upgrade!
 	If you have a weapon, give it +1/+1. Otherwise equip a 1/3 weapon. """
-	#
+	play = (
+		Find(FRIENDLY_WEAPON) &
+		Buff(FRIENDLY_WEAPON, "EX1_409e") |
+		Summon(CONTROLLER, "EX1_409t")
+	)
 	pass
+EX1_409e = buff(+1, +1)
 
 if Classic_Shield_Slam:# 
 	Classic_Warrior+=['VAN_EX1_410']
 class VAN_EX1_410:# <10>[1646]
 	""" Shield Slam
 	Deal 1 damage to a minion for each Armor you have. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Hit(TARGET, ARMOR(FRIENDLY_HERO))
 	pass
 
 if Classic_Gorehowl:# 
@@ -205,54 +212,62 @@ class VAN_EX1_411:# <10>[1646]
 	pass
 
 if Classic_Grommash_Hellscream:# 
-	Classic_Warrior+=['VAN_EX1_414']
+	Classic_Warrior+=['VAN_EX1_414','EX1_414e']
 class VAN_EX1_414:# <10>[1646]
 	""" Grommash Hellscream
 	[Charge][Enrage:] +6 Attack """
-	#
+	enrage = Refresh(SELF, buff="EX1_414e")
 	pass
+EX1_414e = buff(atk=6)
 
 if Classic_Cruel_Taskmaster:# 
-	Classic_Warrior+=['VAN_EX1_603']
+	Classic_Warrior+=['VAN_EX1_603','EX1_603e']
 class VAN_EX1_603:# <10>[1646]
 	""" Cruel Taskmaster
 	[Battlecry:] Deal 1 damage to a minion and give it +2_Attack. """
-	#
+	requirements = {
+		PlayReq.REQ_MINION_TARGET: 0,
+		PlayReq.REQ_NONSELF_TARGET: 0,
+		PlayReq.REQ_TARGET_IF_AVAILABLE: 0}
+	play = Buff(TARGET, "EX1_603e"), Hit(TARGET, 1)
 	pass
+EX1_603e = buff(atk=2)
 
 if Classic_Frothing_Berserker:# 
-	Classic_Warrior+=['VAN_EX1_604']
+	Classic_Warrior+=['VAN_EX1_604','EX1_604o']
 class VAN_EX1_604:# <10>[1646]
 	""" Frothing Berserker
 	Whenever a minion takes damage, gain +1 Attack. """
-	#
+	events = Damage(ALL_MINIONS).on(Buff(SELF, "EX1_604o"))
 	pass
+EX1_604o = buff(atk=1)
 
 if Classic_Shield_Block:# 
 	Classic_Warrior+=['VAN_EX1_606']
 class VAN_EX1_606:# <10>[1646]
 	""" Shield Block
 	Gain 5 Armor.Draw a card. """
-	#
+	play = GainArmor(FRIENDLY_HERO, 5), Draw(CONTROLLER)
 	pass
 
 if Classic_Inner_Rage:# 
-	Classic_Warrior+=['VAN_EX1_607']
+	Classic_Warrior+=['VAN_EX1_607','EX1_607e']
 class VAN_EX1_607:# <10>[1646]
 	""" Inner Rage
 	Deal $1 damage to a minion and give it +2_Attack. """
-	#
+	requirements = {PlayReq.REQ_MINION_TARGET: 0, PlayReq.REQ_TARGET_TO_PLAY: 0}
+	play = Buff(TARGET, "EX1_607e"), Hit(TARGET, 1)
 	pass
+EX1_607e = buff(atk=2)
 
 if Classic_Armor_Up:# 
 	Classic_Warrior+=['VAN_HERO_01bp']
+	Classic_Warrior+=['VAN_HERO_01bp2']
 class VAN_HERO_01bp:# <10>[1646]
 	""" Armor Up!
 	[Hero Power]Gain 2 Armor. """
 	#
 	pass
-
-	Classic_Warrior+=['VAN_HERO_01bp2']
 class VAN_HERO_01bp2:# <10>[1646]
 	""" Tank Up!
 	[Hero Power]Gain 4 Armor. """
@@ -268,10 +283,14 @@ class VAN_NEW1_011:# <10>[1646]
 	pass
 
 if Classic_Commanding_Shout:# 
-	Classic_Warrior+=['VAN_NEW1_036']
+	Classic_Warrior+=['VAN_NEW1_036','NEW1_036e','NEW1_036e2']
 class VAN_NEW1_036:# <10>[1646]
 	""" Commanding Shout
 	Your minions can't be reduced below 1 Health this turn. Draw a card. """
-	#
+	play = Buff(FRIENDLY_MINIONS, "NEW1_036e"), Buff(CONTROLLER, "NEW1_036e2")
 	pass
+NEW1_036e = buff(health_minimum=1)
+class NEW1_036e2:
+	events = Summon(CONTROLLER, MINION).on(Buff(Summon.CARD, "NEW1_036e"))
+
 
