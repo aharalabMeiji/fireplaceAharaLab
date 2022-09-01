ae2db09643fcb9fc8a19176e6237f7765407602c
Kazushi Ahara <ahara@meiji.ac.jp>
2022-09-01 22:47:04 +0900

    Sunken_Shaman half

--- fireplaceAharaLab/fireplace/cards/sunken/sunken_shaman.py
+++ fireplaceAharaLab/fireplace/cards/sunken/sunken_shaman.py
@@ -19,38 +19,61 @@ Sunken_Bioluminescence=True  ###
 if Sunken_Tidelost_Burrower:# 
 	Sunken_Shaman+=['TID_003']
 	Sunken_Shaman+=['TID_003e2']
+class TID_003_DredgeChoice(Choice):
+	def choose(self, card):
+		super().choose(card)
+		if Config.LOGINFO:
+			Config.log("DredgeChoice.choose","%s chooses %r"%(card.controller.name, card))
+		controller = card.controller
+		for c in controller.deck[:3]:
+			if card.id==c.id:
+				controller.deck.remove(c)
+				controller.deck.append(c)
+				if c.type==CardType.MINION and c.race==Race.MURLOC:
+					newcard=Summon(controller, c.id).trigger(controller)
+					newcard=newcard[0][0]
+					Buff(newcard, 'TID_003e2').trigger(controller)
+				break
+		pass
+class TID_003_Dredge(TargetedAction):
+	"""
+	TARGET=ActionArg()#CONTROLLER
+	"""
+	TARGET=ActionArg()
+	def do(self, source, target):
+		bottom3ID=[card.id for card in target.deck[:3]]
+		TID_003_DredgeChoice(target, RandomID(*bottom3ID)*3).trigger(source)
+	pass
 class TID_003:# <8>[1658]
 	""" Tidelost Burrower
 	[Battlecry:] [Dredge].If it's a Murloc, summon a 2/2 copy of it. """
-	#
+	play = TID_003_Dredge(CONTROLLER)
 	pass
-
 class TID_003e2:# <8>[1658]
-	""" Revealed
-	2/2. """
-	#
+	""" Revealed 	2/2. """
+	atk = lambda self,i:2
+	max_health = lambda self,i:2
 	pass
 
 if Sunken_Clownfish:# 
 	Sunken_Shaman+=['TID_004']
-	Sunken_Shaman+=['TID_004e']
+	#Sunken_Shaman+=['TID_004e']
 	Sunken_Shaman+=['TID_004e2']
 class TID_004:# <8>[1658]
 	""" Clownfish
 	[Battlecry:] Your next two Murlocs cost (2) less. """
-	#
+	play = Buff(FRIENDLY_HAND, 'TID_004e2')
 	pass
-
 class TID_004e:# <8>[1658]
 	""" Clownfish Car
 	Your next two Murlocs cost (2) less. """
 	#
 	pass
-
 class TID_004e2:# <8>[1658]
 	""" Clownin' Around
 	Costs (2) less. """
-	#
+	cost = lambda self,i: max(i-2,0)
+	events = Play(CONTROLLER, MURLOC).on(SidequestCounter(SELF, 2, [Destroy(SELF)]))
 	pass
 
 if Sunken_Command_of_Neptulon:# 
@@ -58,14 +81,13 @@ if Sunken_Command_of_Neptulon:#
 	Sunken_Shaman+=['TID_005t']
 class TID_005:# <8>[1658]
 	""" Command of Neptulon
-	Summon two 5/4 Elementals with [Rush].[Overload:] (1) """
-	#
+	Summon two 5/4 Elementals(TID_005t) with [Rush].[Overload:] (1) """
+	#<Tag enumID="215" name="OVERLOAD" type="Int" value="1"/>
+	play = Summon(CONTROLLER, 'TID_005t'), Summon(CONTROLLER, 'TID_005t')
 	pass
-
 class TID_005t:# <8>[1658]
 	""" Water Revenant
 	[Rush] """
-	#
 	pass
 
 if Sunken_Wrathspine_Enchanter:# 
@@ -73,23 +95,35 @@ if Sunken_Wrathspine_Enchanter:#
 class TSC_630:# <8>[1658]
 	""" Wrathspine Enchanter
 	[Battlecry:] Cast a copy of a Fire, Frost, and Nature spell in your hand <i>(targets chosen randomly).</i> """
-	#
+	def play(self):
+		cards=[card for card in self.controller.hand if card.type==CardType.SPELL and card.spell_school==SpellSchool.FIRE]
+		if len(cards)>0:
+			card=random.choice(cards)
+			CastSpell(card.id).trigger(self)
+		cards=[card for card in self.controller.hand if card.type==CardType.SPELL and card.spell_school==SpellSchool.FROST]
+		if len(cards)>0:
+			card=random.choice(cards)
+			CastSpell(card.id).trigger(self)
+		cards=[card for card in self.controller.hand if card.type==CardType.SPELL and card.spell_school==SpellSchool.NATURE]
+		if len(cards)>0:
+			card=random.choice(cards)
+			CastSpell(card.id).trigger(self)
 	pass
 
 if Sunken_Schooling:# 
 	Sunken_Shaman+=['TSC_631']
 class TSC_631:# <8>[1658]
 	""" Schooling
-	Add three 1/1 PiranhaSwarmers to your hand. """
-	#
+	Add three 1/1 Piranha Swarmers(TSC_638) to your hand. """
+	play = Give(CONTROLLER, 'TSC_638') * 3
 	pass
 
 if Sunken_Piranha_Poacher:# 
 	Sunken_Shaman+=['TSC_633']
 class TSC_633:# <8>[1658]
 	""" Piranha Poacher
-	At the end of your turn,add a 1/1 Piranha Swarmer to your hand. """
-	#
+	At the end of your turn,add a 1/1 Piranha Swarmer(TSC_638) to your hand. """
+	events = OWN_TURN_END.on(Give(CONTROLLER, 'TSC_638'))
 	pass
 
 if Sunken_Radiance_of_Azshara:# 
@@ -98,13 +132,15 @@ if Sunken_Radiance_of_Azshara:#
 class TSC_635:# <8>[1658]
 	""" Radiance of Azshara
 	[Fire Spell Damage +2]Your Nature spells cost (1)less. After you cast a Frostspell, gain 3 Armor. """
-	#
+	#<Tag enumID="1946" name="SPELLPOWER_FIRE" type="Int" value="1"/> ## mistake
+	tags = {GameTag.SPELLPOWER_FIRE: 2, }
+	play = Buff(FRIENDLY_HAND + NATURE, 'TSC_635e')
+	events = Play(CONTROLLER, FROST).after(GainArmor(FRIENDLY_HERO, 3))
 	pass
-
 class TSC_635e:# <8>[1658]
 	""" Kaldorei Strength
 	Costs (1) less. """
-	#
+	cost = lambda self, i: max(i-1,0)
 	pass
 
 if Sunken_Scalding_Geyser:# 
@@ -112,7 +148,8 @@ if Sunken_Scalding_Geyser:#
 class TSC_637:# <8>[1658]
 	""" Scalding Geyser
 	Deal $2 damage.[Dredge]. """
-	#
+	requirements = {PlayReq.REQ_TARGET_TO_PLAY:0, PlayReq.REQ_MINION_TARGET:0, PlayReq.REQ_ENEMY_TARGET:0 }
+	play = Hit(TARGET, 2), Dredge(CONTROLLER)
 	pass
 
 if Sunken_Glugg_the_Gulper:# 
