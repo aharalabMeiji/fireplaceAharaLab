2ffa6624d07107fc09d5fdf5da783e0ef1cea282
Kazushi Ahara <ahara@meiji.ac.jp>
2022-10-08 21:41:01 +0900

    Rev_Castle_Kennels:# ### location ###

--- fireplaceAharaLab/fireplace/cards/revendreth/rev_hunter.py
+++ fireplaceAharaLab/fireplace/cards/revendreth/rev_hunter.py
@@ -21,43 +21,57 @@ Rev_Castle_Kennels=True
 
 if Rev_Shadehound:# 
 	Rev_Hunter+=['MAW_009']
+	Rev_Hunter+=['MAW_009e']
+	Rev_Hunter+=['MAW_009t']
 class MAW_009:# <3>[1691]
 	""" Shadehound
-	Whenever this attacks, give your other Beasts +2/+2. <b>Infuse (@ Beasts):</b> Gain <b>Rush</b>. """
-	#
+	Whenever this attacks, give your other Beasts +2/+2. 
+	<b>Infuse (@ Beasts):</b> Gain <b>Rush</b>. """
+	# @ = script_data_num_1 = 3
+	events = Attack(SELF, ENEMY).after(Buff(FRIENDLY_MINIONS + BEAST, 'MAW_009e'))#
+	class Hand:
+		events = Death(FRIENDLY+MINION + BEAST).on(Infuse(CONTROLLER, 'MAW_009t'))
+	class Deck:
+		events = Death(FRIENDLY+MINION + BEAST).on(Infuse(CONTROLLER, 'MAW_009t', 1))
 	pass
-
-	Rev_Hunter+=['MAW_009e']
-class MAW_009e:# <3>[1691]
-	""" Maw Bound
-	+2/+2. """
-	#
-	pass
-
-	Rev_Hunter+=['MAW_009t']
+MAW_009e=buff(2,2)
 class MAW_009t:# <3>[1691]
 	""" Shadehound
 	<b>Infused</b> <b>Rush</b>. Whenever this attacks, give your other Beasts +2/+2. """
-	#
+	events = Attack(SELF, ENEMY).after(Buff(FRIENDLY_MINIONS + BEAST, 'MAW_009e'))#
 	pass
+
+
+
 
 if Rev_Motion_Denied:# 
 	Rev_Hunter+=['MAW_010']
+	Rev_Hunter+=['MAW_010t']
+class c_Action(TargetedAction):
+	CONTROLLER=ActionArg()
+	def do(self, source, controller):
+		pass
 class MAW_010:# <3>[1691]
 	""" Motion Denied
 	<b>Secret:</b> After your opponent plays three cards in a turn, deal $6 damage to the enemy hero. """
-	#
+	secret = 
 	pass
 
-	Rev_Hunter+=['MAW_010t']
 class MAW_010t:# <3>[1691]
 	""" Improved Motion Denied
 	<b>Secret:</b> After your opponent plays three cards in a turn, deal $9 damage to the enemy hero. """
 	#
 	pass
 
+
+
+
 if Rev_Defense_Attorney_Nathanos:# 
 	Rev_Hunter+=['MAW_011']
+class c_Action(TargetedAction):
+	CONTROLLER=ActionArg()
+	def do(self, source, controller):
+		pass
 class MAW_011:# <3>[1691]
 	""" Defense Attorney Nathanos
 	<b>Battlecry:</b> <b>Discover</b> a friendly <b>Deathrattle</b> minion that died this game. Trigger and gain its <b>Deathrattle</b>. """
@@ -71,11 +85,18 @@ class MAW_011e:# <3>[1691]
 	#
 	pass
 
+
+
+
 if Rev_Frenzied_Fangs:# 
 	Rev_Hunter+=['REV_350']
 	Rev_Hunter+=['REV_350e']
 	Rev_Hunter+=['REV_350t']
 	Rev_Hunter+=['REV_350t2']
+class c_Action(TargetedAction):
+	CONTROLLER=ActionArg()
+	def do(self, source, controller):
+		pass
 class REV_350:# <3>[1691]
 	""" Frenzied Fangs
 	Summon two 2/1 Bats. <b>Infuse (@):</b> Give them +1/+2. """
@@ -188,6 +209,9 @@ class REV_353t5:# <3>[1691] Gargon Companions
 	#
 	pass
 
+
+
+
 if Rev_Batty_Guest:# 
 	Rev_Hunter+=['REV_356']
 class REV_356:# <3>[1691]
@@ -196,8 +220,15 @@ class REV_356:# <3>[1691]
 	#
 	pass
 
+
+
+
 if Rev_Spirit_Poacher:# 
 	Rev_Hunter+=['REV_360']
+class c_Action(TargetedAction):
+	CONTROLLER=ActionArg()
+	def do(self, source, controller):
+		pass
 class REV_360:# <3>[1691]
 	""" Spirit Poacher
 	<b>Battlecry:</b> Summon a random <b>Dormant</b> Wildseed. """
@@ -253,73 +284,110 @@ class REV_360te:# <3>[1691]
 	#
 	pass
 
+
+
+
 if Rev_Wild_Spirits:# 
 	Rev_Hunter+=['REV_361']
+class c_Action(TargetedAction):
+	CONTROLLER=ActionArg()
+	def do(self, source, controller):
+		pass
 class REV_361:# <3>[1691]
 	""" Wild Spirits
 	Summon two different  <b>Dormant</b> Wildseeds.  Make your Wildseeds  awaken 1 turn sooner. """
 	#
 	pass
 
-if Rev_Castle_Kennels:# 
+
+
+
+if Rev_Castle_Kennels:# ### location ###
 	Rev_Hunter+=['REV_362']
+	Rev_Hunter+=['REV_362e']
+	Rev_Hunter+=['REV_362e2']
+class REV_362_Action(TargetedAction):
+	TARGET=ActionArg()
+	def do(self, source, target):
+		controller = source.controller
+		Buff(target, 'REV_362e').trigger(source)
+		if target.race==Race.BEAST:
+			target.rush=True
+		pass
 class REV_362:# <3>[1691]
 	""" Castle Kennels
 	Give a friendly minion  +2 Attack. If it's a  Beast, give it <b>Rush</b>. """
-	#
+	requirements = {PlayReq.REQ_TARGET_TO_PLAY:0, PlayReq.REQ_MINION_TARGET:0, PlayReq.REQ_FRIENDLY_TARGET:0 }
+	location = REV_362_Action(TARGET)#
 	pass
+REV_362e=buff(2,0)
+#class REV_362e2:# <3>[1691]
+#	""" Resistant
+#	<b>Immune</b> this turn. """
+#	#
+#	pass
 
-	Rev_Hunter+=['REV_362e']
-class REV_362e:# <3>[1691]
-	""" Beast Mode
-	+2 Attack. """
-	#
-	pass
 
-	Rev_Hunter+=['REV_362e2']
-class REV_362e2:# <3>[1691]
-	""" Resistant
-	<b>Immune</b> this turn. """
-	#
-	pass
+
 
 if Rev_Aralon:# 
 	Rev_Hunter+=['REV_363']
+class c_Action(TargetedAction):
+	CONTROLLER=ActionArg()
+	def do(self, source, controller):
+		pass
 class REV_363:# <3>[1691]
 	""" Ara'lon
 	<b>Battlecry:</b> Summon one of each <b>Dormant</b> Wildseed. """
 	#
 	pass
 
+
+
+
 if Rev_Stag_Charge:# 
 	Rev_Hunter+=['REV_364']
+class c_Action(TargetedAction):
+	CONTROLLER=ActionArg()
+	def do(self, source, controller):
+		pass
 class REV_364:# <3>[1691]
 	""" Stag Charge
 	Deal $3 damage. Summon a random <b>Dormant</b> Wildseed. """
 	#
 	pass
 
+
+
+
 if Rev_Collateral_Damage:# 
 	Rev_Hunter+=['REV_369']
+class c_Action(TargetedAction):
+	CONTROLLER=ActionArg()
+	def do(self, source, controller):
+		pass
 class REV_369:# <3>[1691]
 	""" Collateral Damage
 	Deal $6 damage to three  random enemy minions.  Excess damage hits  the enemy hero. """
 	#
 	pass
 
-if Rev_Aralon:# 
-	Rev_Hunter+=['REV_780']
-class REV_780:# <3>[1691]
-	""" Ara'lon
-	{0} {1} {2} {3} """
-	#
-	pass
 
-if Rev_Castle_Kennels:# 
-	Rev_Hunter+=['REV_790']
-class REV_790:# <3>[1691]
-	""" Castle Kennels
-	{0} {1} """
-	#
-	pass
+
+
+#if Rev_Aralon:# 
+#	Rev_Hunter+=['REV_780']
+#class REV_780:# <3>[1691]
+#	""" Ara'lon
+#	{0} {1} {2} {3} """
+#	#
+#	pass
+
+#if Rev_Castle_Kennels:# 
+#	Rev_Hunter+=['REV_790']
+#class REV_790:# <3>[1691]
+#	""" Castle Kennels
+#	{0} {1} """
+#	#
+#	pass
 
