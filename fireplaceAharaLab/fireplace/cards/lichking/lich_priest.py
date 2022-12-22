081758177776b9d065b0ce8a9f9cc65578c5c826
Kazushi Ahara <ahara@meiji.ac.jp>
2022-12-22 21:05:03 +0900

    Lich_Priest

--- fireplaceAharaLab/fireplace/cards/lichking/lich_priest.py
+++ fireplaceAharaLab/fireplace/cards/lichking/lich_priest.py
@@ -19,11 +19,15 @@ if Lich_Animate_Dead:#
 class RLK_812_Action(GameAction):# 
 	def do(self, source):# 
 		controller=source.controller
+		cards=[card.id for card in controller.death_log if card.type==CardType.MINION and card.cost<=3 ]
+		if len(cards):
+			card=random.choice(cards)
+			Summon(controller, card).trigger(source)
 		pass
 class RLK_812:# <6>[1776]
 	""" Animate Dead (spell:1)
 	Resurrect a friendly minion that costs (3) or less. """
-	#
+	play = RLK_812_Action()
 	pass
 
 if Lich_Bonecaller:# 
@@ -31,73 +35,62 @@ if Lich_Bonecaller:#
 class RLK_813_Action(GameAction):# 
 	def do(self, source):# 
 		controller=source.controller
+		cards=[card.id for card in controller.death_log if card.type==CardType.MINION and card.race==Race.UNDEAD ]
+		if len(cards):
+			card=random.choice(cards)
+			Summon(controller, card).trigger(source)
 		pass
 class RLK_813:# <6>[1776]
 	""" Bonecaller (minion:4/2/4)
 	<b>Taunt</b> <b>Deathrattle</b>: Resurrect a friendly Undead that died this game. """
-	#
+	play = RLK_813_Action()
 	pass
 
 if Lich_Crystalsmith_Cultist:# 
 	Lich_Priest+=['RLK_814']
-class RLK_814_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
+	Lich_Priest+=['RLK_814e']
 class RLK_814:# <6>[1776]
 	""" Crystalsmith Cultist (minion:1/1/2)
 	<b>Battlecry:</b> If you're holding a Shadow spell, gain +1/+1. """
-	#
+	play = Find(FRIENDLY_HAND + SPELL + SHADOW) & Buff(SELF, 'RLK_814e')
 	pass
-
-	Lich_Priest+=['RLK_814e']
-class RLK_814e_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
-class RLK_814e:# <6>[1776]
-	""" Shadow Flow (0)
-	+1/+1 """
-	#
-	pass
+RLK_814e=buff(1,1)
+""" Shadow Flow (0)	+1/+1 """
 
 if Lich_Shadow_Word_Undeath:# 
 	Lich_Priest+=['RLK_815']
 class RLK_815_Action(GameAction):# 
 	def do(self, source):# 
 		controller=source.controller
+		cards=[card.id for card in controller.death_until_last_turn if card.type==CardType.MINION and card.race==Race.UNDEAD ]
+		if len(cards):
+			Hit(ENEMY_CHARACTERS, 2).trigger(source)
 		pass
 class RLK_815:# <6>[1776]
 	""" Shadow Word: Undeath (spell:5)
 	Deal $2 damage to all enemies. If a friendly Undead died after your last turn, deal $2 more. """
-	#
+	play = Hit(ENEMY_CHARACTERS, 2), RLK_815_Action()
 	pass
 
 if Lich_Sister_Svalna:# 
 	Lich_Priest+=['RLK_816']
-class RLK_816_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
+	Lich_Priest+=['RLK_816t3']
 class RLK_816:# <6>[1776]
 	""" Sister Svalna (minion:6/6/6)
-	<b>Battlecry:</b> <i>Permanently</i> add a Vision of Darkness to your hand. """
-	#
+	<b>Battlecry:</b> <i>Permanently</i> add a Vision of Darkness(RLK_816t3) to your hand. """
+	play = Give(CONTROLLER, 'RLK_816t3')
 	pass
-
-	Lich_Priest+=['RLK_816t3']
-class RLK_816t3_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_816t3:# <6>[1776]
 	""" Vision of Darkness (spell:3)
 	<b>Discover</b> a Shadow spell. <i>(This stays in your hand.)</i> """
-	#
+	play = Discover(CONTROLLER, RandomSpell()), Give(CONTROLLER, 'RLK_816')
 	pass
 
 if Lich_Haunting_Nightmare:# 
 	Lich_Priest+=['RLK_822']
+	Lich_Priest+=['RLK_822e']
+	Lich_Priest+=['RLK_822e2']
+	Lich_Priest+=['RLK_822t']
 class RLK_822_Action(GameAction):# 
 	def do(self, source):# 
 		controller=source.controller
@@ -105,40 +98,20 @@ class RLK_822_Action(GameAction):#
 class RLK_822:# <6>[1776]
 	""" Haunting Nightmare (minion:3/3/3)
 	<b>Deathrattle:</b> Haunt a card in your hand. When you play it, summon a 3/3 Soldier. """
-	#
+	deathrattle = Buff(RANDOM(FRIENDLY_HAND, 'RLK_822e'))
 	pass
-
-	Lich_Priest+=['RLK_822e']
-class RLK_822e_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_822e:# <6>[1776]
 	""" Cold Sweat (0)
 	 """
-	#
+	events = Play(CONTROLLER, OWNER).on(Summon(CONTROLLER, 'RLK_822t'), Destroy(SELF))
 	pass
-
-	Lich_Priest+=['RLK_822e2']
-class RLK_822e2_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_822e2:# <6>[1776]
 	""" Night Terror (0)
 	Summons a 3/3 Soldier """
-	#
 	pass
-
-	Lich_Priest+=['RLK_822t']
-class RLK_822t_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_822t:# <6>[1776]
 	""" Haunted Soldier (minion:3/3/3)
 	 """
-	#
 	pass
 
 if Lich_Undying_Allies:# 
