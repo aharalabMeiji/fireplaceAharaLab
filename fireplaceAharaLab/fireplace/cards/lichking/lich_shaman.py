87223f965eda70dd5ed489a2cc791500a1bd7c8f
Kazushi Ahara <ahara@meiji.ac.jp>
2022-12-24 07:59:01 +0900

    Lich_Shaman

--- fireplaceAharaLab/fireplace/cards/lichking/lich_shaman.py
+++ fireplaceAharaLab/fireplace/cards/lichking/lich_shaman.py
@@ -1,3 +1,4 @@
+from telnetlib import GA
 from ..utils import *
 
 Lich_Shaman=[]
@@ -16,37 +17,22 @@ Lich_Overlord_Drakuru=True
 
 if Lich_Rotgill:# 
 	Lich_Shaman+=['RLK_550']
-class RLK_550_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
+	Lich_Shaman+=['RLK_550e']
+	Lich_Shaman+=['RLK_550e2']
 class RLK_550:# <8>[1776]
 	""" Rotgill (minion:5/3/6)
 	<b>Battlecry:</b> Give your other minions "<b>Deathrattle:</b> Give __your minions +1/+1." """
-	#
+	play = Buff(FRIENDLY_MINIONS - SELF, 'RLK_550e')
 	pass
-
-	Lich_Shaman+=['RLK_550e']
-class RLK_550e_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_550e:# <8>[1776]
 	""" Deathwatch (0)
 	<b>Deathrattle:</b> Give your minions +1/+1. """
-	#
+	tags = {GameTag.DEATHRATTLE:True, }
+	deathrattle = Buff(FRIENDLY_MINIONS, 'RLK_550e2')
 	pass
+RLK_550e2=buff(1,1)
+""" Deathsight (0)	+1/+1. """
 
-	Lich_Shaman+=['RLK_550e2']
-class RLK_550e2_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
-class RLK_550e2:# <8>[1776]
-	""" Deathsight (0)
-	+1/+1. """
-	#
-	pass
 
 if Lich_Blightblood_Berserker:# 
 	Lich_Shaman+=['RLK_551']
@@ -57,38 +43,44 @@ class RLK_551_Action(GameAction):#
 class RLK_551:# <8>[1776]
 	""" Blightblood Berserker (minion:8/3/8)
 	<b>Taunt</b>, <b>Lifesteal</b>, <b>Reborn</b> <b>Deathrattle:</b> Deal 3 damage to a random enemy. """
-	#
+	deathrattle = Hit(RANDOM(ENEMY_CHARACTERS), 3)
 	pass
+
 
 if Lich_Unliving_Champion:# 
 	Lich_Shaman+=['RLK_552']
 class RLK_552_Action(GameAction):# 
 	def do(self, source):# 
 		controller=source.controller
+		cards = [card for card in controller.death_after_last_turn if card.type==CardType.MINION and card.race==Race.UNDEAD]
+		if len(cards):
+			Summon(controller, 'RLK_909t').trigger(source)
 		pass
 class RLK_552:# <8>[1776]
 	""" Unliving Champion (minion:3/3/2)
-	<b>Battlecry:</b> If a friendly Undead died after your last turn, summon two 3/2 Zombies. """
-	#
+	<b>Battlecry:</b> If a friendly Undead died after your last turn, summon two 3/2 Zombies(RLK_909t). """
+	play = RLK_552_Action()
 	pass
+
 
 if Lich_Prescience:# 
 	Lich_Shaman+=['RLK_553']
+	Lich_Shaman+=['RLK_553t']
 class RLK_553_Action(GameAction):# 
 	def do(self, source):# 
 		controller=source.controller
+		card1=get00(Draw(controller).trigger(source))
+		card2=get00(Draw(controller).trigger(source))
+		if card1.cost>=5:
+			Summon(controller, 'RLK_553t').trigger(source)
+		if card2.cost>=5:
+			Summon(controller, 'RLK_553t').trigger(source)
 		pass
 class RLK_553:# <8>[1776]
 	""" Prescience (spell:4)
-	Draw 2 minions. For each that costs (5) or more, summon a_ 2/3 Spirit with <b>Taunt</b>. """
-	#
+	Draw 2 minions. For each that costs (5) or more, summon a_ 2/3 Spirit(RLK_553t) with <b>Taunt</b>. """
+	play = RLK_553_Action()
 	pass
-
-	Lich_Shaman+=['RLK_553t']
-class RLK_553t_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_553t:# <8>[1776]
 	""" Ghastly Apparition (minion:2/2/3)
 	<b>Taunt</b> """
@@ -97,29 +89,22 @@ class RLK_553t:# <8>[1776]
 
 if Lich_Harkener_of_Dread:# 
 	Lich_Shaman+=['RLK_554']
-class RLK_554_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
+	Lich_Shaman+=['RLK_554t']
 class RLK_554:# <8>[1776]
 	""" Harkener of Dread (minion:5/2/2)
 	<b>Taunt</b> <b>Deathrattle:</b> Summon a 6/6 Undead with <b>Taunt</b>. """
-	#
+	deathrattle = Summon(CONTROLLER, 'RLK_554t')
 	pass
-
-	Lich_Shaman+=['RLK_554t']
-class RLK_554t_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_554t:# <8>[1776]
 	""" Drakkari Specter (minion:6/6/6)
 	<b>Taunt</b> """
-	#
 	pass
+
 
 if Lich_Deathweaver_Aura:# 
 	Lich_Shaman+=['RLK_909']
+	Lich_Shaman+=['RLK_909e']
+	Lich_Shaman+=['RLK_909t']
 class RLK_909_Action(GameAction):# 
 	def do(self, source):# 
 		controller=source.controller
@@ -127,30 +112,21 @@ class RLK_909_Action(GameAction):#
 class RLK_909:# <8>[1776]
 	""" Deathweaver Aura (spell:2)
 	Give a minion "<b>Deathrattle:</b> Summon two 3/2 Zombies." <b>Overload:</b> (1) """
-	#
+	#<Tag enumID="215" name="OVERLOAD" type="Int" value="1"/>
+	requirements = REQUIRE_FRIEND_MINION_TARGET
+	play = Buff(TARGET, 'RLK_909e')
 	pass
-
-	Lich_Shaman+=['RLK_909e']
-class RLK_909e_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_909e:# <8>[1776]
 	""" Voodoo Be With You (0)
 	<b>Deathrattle:</b> Summon two 3/2 Zombies. """
-	#
+	tags = {GameTag.DEATHRATTLE:True, }
+	deathrattle = Summon(CONTROLLER, 'RLK_909t'), Summon(CONTROLLER, 'RLK_909t')
 	pass
-
-	Lich_Shaman+=['RLK_909t']
-class RLK_909t_Action(GameAction):# 
-	def do(self, source):# 
-		controller=source.controller
-		pass
 class RLK_909t:# <8>[1776]
 	""" Drakkari Zombie (minion:2/3/2)
 	 """
-	#
 	pass
+
 
 if Lich_Shadow_Suffusion:# 
 	Lich_Shaman+=['RLK_910']
